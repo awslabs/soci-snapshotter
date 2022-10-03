83a0c1fd2da62ffcde9483cfdd67cbbe64cd9228
Kern Walster <walster@amazon.com>
2022-10-03 17:07:02 +0000

    Remote `soci image list-indices`
    `soci image list-indices` was deprecated in favor of `soci index list
    --ref`, however the command wasn't removed because the output format was
    convenient for integration tests. Now that `soci index list -q` outputs
    in the same format, we can remove the dead command.
    
    Signed-off-by: Kern Walster <walster@amazon.com>

--- integration/pull_test.go
+++ integration/pull_test.go
@@ -832,8 +832,8 @@ func buildSparseIndex(sh *shell.Shell, src imageInfo, minLayerSize int64) string
 	indexDigest := sh.
 		X(append([]string{"ctr", "i", "pull"}, opts[0]...)...).
 		X("soci", "create", src.ref, "--min-layer-size", fmt.Sprintf("%d", minLayerSize), "--oras").
-		O("soci", "image", "list-indices", src.ref) // this will make SOCI artifact available locally
-	return string(indexDigest)
+		O("soci", "index", "list", "-q", "--ref", src.ref) // this will make SOCI artifact available locally
+	return strings.Trim(string(indexDigest), "\n")
 }
 
 func rebootContainerd(t *testing.T, sh *shell.Shell, customContainerdConfig, customSnapshotterConfig string) *testutil.RemoteSnapshotMonitor {
