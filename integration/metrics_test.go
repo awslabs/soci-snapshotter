2fbb32115aadea5063c5ee9fa20e41aee271001b
Rishabh Singhvi <rdpsin@amazon.com>
2023-01-03 17:07:58 +0000

    Add metrics for overlayfs fallback
    This commit adds a metric `fuse_mount_failure_count` for when the snapshotter falls back to using
    overlayfs instead of FUSE mount for mounting the layer.
    
    Note that a layer not having a corresponding ztoc is NOT classified as
    an error, even though `fs.Mount` returns an error in that case and the
    layer is mounted as a normal overlayfs mount.
    
    Signed-off-by: Rishabh Singhvi <rdpsin@amazon.com>

--- integration/metrics_test.go
+++ integration/metrics_test.go
@@ -16,7 +16,15 @@
 
 package integration
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	commonmetrics "github.com/awslabs/soci-snapshotter/fs/metrics/common"
+	shell "github.com/awslabs/soci-snapshotter/util/dockershell"
+	"github.com/awslabs/soci-snapshotter/util/testutil"
+)
 
 const (
 	tcpMetricsAddress  = "localhost:1338"
@@ -63,4 +71,124 @@ func TestMetrics(t *testing.T) {
 		})
 	}
 
+}
+
+func TestOverlayFallbackMetric(t *testing.T) {
+	regConfig := newRegistryConfig()
+	getContainerdConfigYaml := func(disableVerification bool) []byte {
+		additionalConfig := ""
+		if !isTestingBuiltinSnapshotter() {
+			additionalConfig = proxySnapshotterConfig
+		}
+		return []byte(testutil.ApplyTextTemplate(t, `
+version = 2
+
+[plugins."io.containerd.snapshotter.v1.soci"]
+root_path = "/var/lib/soci-snapshotter-grpc/"
+disable_verification = {{.DisableVerification}}
+
+[plugins."io.containerd.snapshotter.v1.soci".blob]
+check_always = true
+
+[debug]
+format = "json"
+level = "debug"
+
+{{.AdditionalConfig}}
+`, struct {
+			DisableVerification bool
+			AdditionalConfig    string
+		}{
+			DisableVerification: disableVerification,
+			AdditionalConfig:    additionalConfig,
+		}))
+	}
+	getSnapshotterConfigYaml := func(disableVerification bool) []byte {
+		return []byte(fmt.Sprintf(`
+metrics_address = "localhost:6060"
+disable_verification = %v
+`, disableVerification))
+	}
+
+	sh, done := newShellWithRegistry(t, regConfig)
+	defer done()
+
+	if err := testutil.WriteFileContents(sh, defaultContainerdConfigPath, getContainerdConfigYaml(false), 0600); err != nil {
+		t.Fatalf("failed to write %v: %v", defaultContainerdConfigPath, err)
+	}
+	if err := testutil.WriteFileContents(sh, defaultSnapshotterConfigPath, getSnapshotterConfigYaml(false), 0600); err != nil {
+		t.Fatalf("failed to write %v: %v", defaultSnapshotterConfigPath, err)
+	}
+
+	testCases := []struct {
+		name                  string
+		image                 string
+		indexDigestFn         func(*shell.Shell, imageInfo) string
+		expectedFallbackCount int
+	}{
+		{
+			name:  "image with all layers having ztocs and no fs.Mount error results in 0 overlay fallback",
+			image: "rabbitmq:latest",
+			indexDigestFn: func(sh *shell.Shell, image imageInfo) string {
+				return optimizeImageWithOpts(sh, image, 1<<22, 0)
+			},
+			expectedFallbackCount: 0,
+		},
+		{
+			name:  "image with some layers not having ztoc and no fs.Mount results in 0 overlay fallback",
+			image: "rabbitmq:latest",
+			indexDigestFn: func(sh *shell.Shell, image imageInfo) string {
+				return optimizeImageWithOpts(sh, image, 1<<22, 10<<20)
+			},
+			expectedFallbackCount: 0,
+		},
+		{
+			name:  "image with fs.Mount errors results in non-zero overlay fallback",
+			image: "rabbitmq:latest",
+			indexDigestFn: func(_ *shell.Shell, _ imageInfo) string {
+				return "dwadwadawdad"
+			},
+			expectedFallbackCount: 10,
+		},
+	}
+
+	checkOverlayFallbackCount := func(output string, expected int) error {
+		lines := strings.Split(output, "\n")
+		for _, line := range lines {
+			if !strings.Contains(line, commonmetrics.FuseMountFailureCount) {
+				continue
+			}
+			var got int
+			_, err := fmt.Sscanf(line, `soci_fs_operation_count{layer="",operation_type="fuse_mount_failure_count"} %d`, &got)
+			if err != nil {
+				return err
+			}
+			if got != expected {
+				return fmt.Errorf("unexpected overlay fallbacks: got %d, expected %d", got, expected)
+			}
+			return nil
+		}
+		if expected != 0 {
+			return fmt.Errorf("expected %d overlay fallbacks but got 0", expected)
+		}
+		return nil
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rebootContainerd(t, sh, "", "")
+
+			imgInfo := dockerhub(tc.image)
+
+			sh.X("ctr", "i", "pull", imgInfo.ref)
+			indexDigest := tc.indexDigestFn(sh, imgInfo)
+
+			sh.X("soci", "image", "rpull", "--soci-index-digest", indexDigest, imgInfo.ref)
+			curlOutput := string(sh.O("curl", "localhost:6060/metrics"))
+
+			if err := checkOverlayFallbackCount(curlOutput, tc.expectedFallbackCount); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
 }
