6d83eef8cadf1c299050cd539b002fd19a0fe99f
Jin Dong <jindon@amazon.com>
2023-01-31 14:55:37 +0000

    Clean up makefile
    Signed-off-by: Jin Dong <jindon@amazon.com>

---
+++ scripts/install-check-tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#   Copyright The containerd Authors.
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+echo "Install soci check tools"
+set -eux -o pipefail
+
+curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.49.0
+go install github.com/kunalkushwaha/ltag@v0.2.3
+go install github.com/vbatts/git-validation@v1.1.0
