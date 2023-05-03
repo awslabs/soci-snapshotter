2d50e48e8e66745af57a223b8a012e2d5be5d340
Kern Walster <walster@amazon.com>
2023-05-03 21:57:26 +0000

    Add uncompressed archive size check to ztoc tests
    Signed-off-by: Kern Walster <walster@amazon.com>

--- ztoc/toc_builder.go
+++ ztoc/toc_builder.go
@@ -184,9 +184,7 @@ type positionTrackerReader struct {
 
 func (p *positionTrackerReader) Read(b []byte) (int, error) {
 	n, err := p.r.Read(b)
-	if err == nil {
-		p.pos += compression.Offset(n)
-	}
+	p.pos += compression.Offset(n)
 	return n, err
 }
 
