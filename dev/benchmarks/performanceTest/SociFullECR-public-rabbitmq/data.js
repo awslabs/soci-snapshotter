822849394e1aa4b2276cdf84dda3c7e728c2479e
github-action-benchmark <github@users.noreply.github.com>
2023-08-14 18:46:27 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 184d1715fe4985936018f8013dd81c54019ae4e4

---
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -0,0 +1,49 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1692038787111,
+  "repoUrl": "https://github.com/awslabs/soci-snapshotter",
+  "entries": {
+    "Soci Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun Yogidas"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "184d1715fe4985936018f8013dd81c54019ae4e4",
+          "message": "Add benchmark visualization workflow\n\nThis commit changes the benchmark.yml workflow into\nbenchmark_visualization.yml. The new workflow runs on every code merge,\nit will run the benchmark target and upload the result as\nbenchmark-result-artifact. The results are then converted to the\nappropriate format for visualization using the\nvisualization_data_converter.sh shell script. It then uploads the newly\ngenerated data files to Github-pages using the github-action-benchmark\naction.\n\nSigned-off-by: Arjun Raja Yogidas <arjunry@amazon.com>",
+          "timestamp": "2023-08-14T14:24:40-04:00",
+          "tree_id": "4d49c9a79b3c29a9a58706a04dc54cbdfcd909e7",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/184d1715fe4985936018f8013dd81c54019ae4e4"
+        },
+        "date": 1692038786576,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 7.367,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 7.38,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 8.5195,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      }
+    ]
+  }
+}
