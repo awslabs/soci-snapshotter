716e23eae15516582837535398a3e888793002c4
github-action-benchmark <github@users.noreply.github.com>
2023-08-22 20:06:43 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 5a40aff504535a863e0655de76a77b058184cafc

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692726135359,
+  "lastUpdate": 1692734803758,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -205,6 +205,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 75.44749999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manujgrover71@gmail.com",
+            "name": "Manuj Grover",
+            "username": "manujgrover71"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "5a40aff504535a863e0655de76a77b058184cafc",
+          "message": "Using standard error package instead of go-multierror for multierror error.\n\nSigned-off-by: Manuj Grover <manujgrover71@gmail.com>",
+          "timestamp": "2023-08-22T15:49:08-04:00",
+          "tree_id": "b3c836a33b9dbca10ef7d1afb88e4e343a05e9b3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/5a40aff504535a863e0655de76a77b058184cafc"
+        },
+        "date": 1692734803224,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 3.978,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.542,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 91.2995,
             "unit": "Seconds",
             "extra": "P90"
           }
