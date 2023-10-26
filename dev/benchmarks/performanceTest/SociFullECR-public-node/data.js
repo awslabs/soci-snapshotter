e0b800ad6213eaec26326155a5ae47167db49a61
github-action-benchmark <github@users.noreply.github.com>
2023-10-26 15:40:42 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 83aa1d02e0b22e5ff7e0f90ff071080d02ca6a03

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698194351497,
+  "lastUpdate": 1698334842045,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1129,6 +1129,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 11.4955,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "83aa1d02e0b22e5ff7e0f90ff071080d02ca6a03",
+          "message": "Update scripts with minor changes\n\nThis changeset adds consistent naming to shell scripts, adds copyrights\nto benchmark scripts, and changes shebangs to env bash to work in\nenvironments when bash is not under `/bin/`.\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2023-10-26T08:21:36-07:00",
+          "tree_id": "ebc50b2a859f3b208acc286de68f54345b856878",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/83aa1d02e0b22e5ff7e0f90ff071080d02ca6a03"
+        },
+        "date": 1698334840260,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 0.4045,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 9.498000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
