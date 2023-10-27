028b6899e9336da5d020c96ceaeedc612fc9ccf9
github-action-benchmark <github@users.noreply.github.com>
2023-10-27 17:47:00 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 49dc6a77a41175bca0dc74d43389c61edc171803

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698348442368,
+  "lastUpdate": 1698428820307,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1213,6 +1213,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 59.5885,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "49dc6a77a41175bca0dc74d43389c61edc171803",
+          "message": "Add test against referrers API\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2023-10-27T10:31:47-07:00",
+          "tree_id": "e9e58433bb8cd7b8480baf068fcd6a1d950a3d66",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/49dc6a77a41175bca0dc74d43389c61edc171803"
+        },
+        "date": 1698428816890,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 0.4005,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.3945,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 10.664,
             "unit": "Seconds",
             "extra": "P90"
           }
