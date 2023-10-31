08bd6ce1d82777dfbaa7aa4e0eb845f51618a927
github-action-benchmark <github@users.noreply.github.com>
2023-10-31 21:35:12 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7557b6c3bcf91092c52f38e6f02afdbb38e54a32

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698774944780,
+  "lastUpdate": 1698788112619,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1381,6 +1381,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 10.8695,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "7557b6c3bcf91092c52f38e6f02afdbb38e54a32",
+          "message": "Remove most references to ctr\n\nRemoved all references except from \"image rpull\" and \"run\".\n\"run\" is entirely delegated to ctr and thus probably not worth porting.\n\"image rpull\" uses more than just flags and might be a tad more\ndifficult to port over.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2023-10-31T14:19:54-07:00",
+          "tree_id": "0ee27429a49842b9155c2a9279cbcb0dcdd63dce",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7557b6c3bcf91092c52f38e6f02afdbb38e54a32"
+        },
+        "date": 1698788110449,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 0.401,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.404,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 10.9065,
             "unit": "Seconds",
             "extra": "P90"
           }
