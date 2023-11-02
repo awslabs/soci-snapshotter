b532a23172119446e184755897679c0618aa128b
github-action-benchmark <github@users.noreply.github.com>
2023-11-02 17:45:08 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for ca55a07e2811d7fc0319b349a81990b2343d2bc6

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698871919029,
+  "lastUpdate": 1698947108595,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1549,6 +1549,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 81.994,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "ca55a07e2811d7fc0319b349a81990b2343d2bc6",
+          "message": "Bump github.com/containerd/containerd\n\nThe previous dependabot commit somehow downgraded containerd.\nThis commit should bring it back to v1.7.8.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2023-11-02T10:27:22-07:00",
+          "tree_id": "dd6550fe1c88b677d6112409f2def6affc495f03",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/ca55a07e2811d7fc0319b349a81990b2343d2bc6"
+        },
+        "date": 1698947106922,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 3.616,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.083,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 72.714,
             "unit": "Seconds",
             "extra": "P90"
           }
