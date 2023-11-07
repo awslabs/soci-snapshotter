1966ad8bb8c6bec57347f9c426374cf25bebe5b5
github-action-benchmark <github@users.noreply.github.com>
2023-11-07 21:05:08 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 192b50eb6b16809f604599585a3a2c7004ac3aa9

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699052339029,
+  "lastUpdate": 1699391108592,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1799,6 +1799,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 0.877,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "turyasin@amazon.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "192b50eb6b16809f604599585a3a2c7004ac3aa9",
+          "message": "Bump go-fuse to v2.4.1\n\nUpgrading go-fuse to a tagged revision which contains\nthe bug fix for the sparse file `cp/mv/install` bug on\nimages with coreutils version >= v9.0.\n\nSigned-off-by: Yasin Turan <turyasin@amazon.com>",
+          "timestamp": "2023-11-07T15:50:32-05:00",
+          "tree_id": "b050ecf4574d99b17b50b607ee525c572b24c535",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/192b50eb6b16809f604599585a3a2c7004ac3aa9"
+        },
+        "date": 1699391107979,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 6.98,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.908,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 3.5415,
             "unit": "Seconds",
             "extra": "P90"
           }
