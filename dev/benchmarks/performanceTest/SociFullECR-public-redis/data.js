2af4860b5da11dc816a5396cbc4ef4de9d9bf052
github-action-benchmark <github@users.noreply.github.com>
2023-11-08 20:02:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f25e4591adb3524981f2dafb19b196453d7f201a

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699391112955,
+  "lastUpdate": 1699473742065,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1923,6 +1923,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 2.4085,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "f25e4591adb3524981f2dafb19b196453d7f201a",
+          "message": "Allow benchmarks config as json\n\nBenchmarks can currently be passed as a csv file. This additionally adds\njson as an option to make it not order dependent and to allow us to add\nmore complex options in the future (e.g. mounts).\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2023-11-08T11:52:53-08:00",
+          "tree_id": "6900591438deee7739ff3aa04b4f6977d1ca789e",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f25e4591adb3524981f2dafb19b196453d7f201a"
+        },
+        "date": 1699473741216,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 3.7495,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.020999999999999998,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.042,
             "unit": "Seconds",
             "extra": "P90"
           }
