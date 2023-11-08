4136d0333f524b584afa5eefe795e0f235e1dfa7
github-action-benchmark <github@users.noreply.github.com>
2023-11-08 20:09:02 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for bc17f0851c97218562bf7786a940f37904a44758

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699473749102,
+  "lastUpdate": 1699474142295,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1760,6 +1760,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.0095,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "bc17f0851c97218562bf7786a940f37904a44758",
+          "message": "Remove \"soci run\" from CLI\n\nWith the effort to remove ctr code from CLI, we removed `soci run`,\nas it ran `ctr run` under the hood with no additional functionality.\nIn the testing suite, `soci run` has been replaced in favor of\n`nerdctl run` for similar reasons to above.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2023-11-08T12:00:37-08:00",
+          "tree_id": "eb0f665e3fc6779665c3c9061751ae4ed663d772",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/bc17f0851c97218562bf7786a940f37904a44758"
+        },
+        "date": 1699474141773,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 14.813500000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 7.4475,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 0.899,
             "unit": "Seconds",
             "extra": "P90"
           }
