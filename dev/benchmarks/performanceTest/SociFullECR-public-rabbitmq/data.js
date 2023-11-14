0bdebf5b7a2e29bef6b95efb14983e91d9dbc721
github-action-benchmark <github@users.noreply.github.com>
2023-11-14 19:45:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 8dfb7071444c53c3bbdc8c10cbc45eb940186589

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699474351011,
+  "lastUpdate": 1699991122066,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1844,6 +1844,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 2.3805,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "8dfb7071444c53c3bbdc8c10cbc45eb940186589",
+          "message": "Switch release workflow to on.push\n\nBefore this change, the release workflow was running when creating a\nbranch.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2023-11-14T11:35:24-08:00",
+          "tree_id": "26af82720df0e8873c20a4fa461902ad82469d07",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/8dfb7071444c53c3bbdc8c10cbc45eb940186589"
+        },
+        "date": 1699991116367,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 10.898,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 7.599,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 0.895,
             "unit": "Seconds",
             "extra": "P90"
           }
