eabf59957aca90acbe43b2c30cdfb51b612cb1b6
github-action-benchmark <github@users.noreply.github.com>
2023-11-15 19:40:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for bfd6bb6112413e647db6e5625628de2a842a8a10

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700024502560,
+  "lastUpdate": 1700077222394,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -2133,6 +2133,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.2974999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@tuananh.org",
+            "name": "Tuan Anh Tran",
+            "username": "tuananh"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "bfd6bb6112413e647db6e5625628de2a842a8a10",
+          "message": "feat: export emptyindex error when ztoc empty\n\nSigned-off-by: Tuan Anh Tran <me@tuananh.org>",
+          "timestamp": "2023-11-15T14:31:51-05:00",
+          "tree_id": "76509bc88bb8da16a7313a0d98683f7ba74713b2",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/bfd6bb6112413e647db6e5625628de2a842a8a10"
+        },
+        "date": 1700077217943,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 14.9815,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.2175000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.2805,
             "unit": "Seconds",
             "extra": "P90"
           }
