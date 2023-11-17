5e468ed0eaaba439fb27a55316920e62bc872f7a
github-action-benchmark <github@users.noreply.github.com>
2023-11-17 16:14:52 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for e1c66c057b5de0afb278058519ea20844309504a

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700154382325,
+  "lastUpdate": 1700237692677,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -2095,6 +2095,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.7229999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "e1c66c057b5de0afb278058519ea20844309504a",
+          "message": "Redact HTTP query values from logs\n\nHTTP client logs are mostly disabled with the exception of a request\nretry log. The issue observed is that error messages may contain the\nfull HTTP request including the query component which can contain\nsensitive information like credentials or session tokens. To prevent\nleaking sensitive information, this change will redact HTTP query values\nfrom log messages.\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2023-11-17T11:00:27-05:00",
+          "tree_id": "4f20d050b4267fdd5f9eee60638de4d5c7b4d540",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/e1c66c057b5de0afb278058519ea20844309504a"
+        },
+        "date": 1700237690331,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 8.8955,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4625,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.816,
             "unit": "Seconds",
             "extra": "P90"
           }
