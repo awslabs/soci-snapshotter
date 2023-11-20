a394e442f061a4bf39924a647121cbe5f5bd4e3d
github-action-benchmark <github@users.noreply.github.com>
2023-11-20 16:27:43 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 6dcfca7bc0c70b00a5f23813d468379d6fbc8de5

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700269958326,
+  "lastUpdate": 1700497663438,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -2261,6 +2261,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 0.863,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "turyasin@amazon.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "6dcfca7bc0c70b00a5f23813d468379d6fbc8de5",
+          "message": "Support re-authentication on 403\n\nAlthough in most cases 403 responses represent authorization\nissues that generally cannot be resolved by re-authentication,\nsome registries like ECR, will return a 403 on credential\nexpiration. We will attempt to re-authenticate only if the\nresponse body indicates credential expiration.\n\nRef: https://docs.aws.amazon.com/AmazonECR/latest/userguide/common-errors-docker.html#error-403\n\nSigned-off-by: Yasin Turan <turyasin@amazon.com>",
+          "timestamp": "2023-11-20T11:16:12-05:00",
+          "tree_id": "ea5f244f00095de13836e211f3bf733f3d7157ab",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/6dcfca7bc0c70b00a5f23813d468379d6fbc8de5"
+        },
+        "date": 1700497662857,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 5.1419999999999995,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.296,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.633,
             "unit": "Seconds",
             "extra": "P90"
           }
