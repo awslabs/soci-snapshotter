44a669cbc173c5276a14ffab9e2432311d6881bb
github-action-benchmark <github@users.noreply.github.com>
2023-12-07 16:51:10 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for cf25e82c4f729232981c5146890055da518007b3

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701290577556,
+  "lastUpdate": 1701967870491,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -2511,6 +2511,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 0.7735000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "turyasin@amazon.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "cf25e82c4f729232981c5146890055da518007b3",
+          "message": "Store file name as is in metadata DB\n\nRight now, when converting the TOC to metadata entries, we call `path.Clean`\non every file name before writing it to metadata DB. Calling `path.Clean`\non a directory path removes the trailing separator. This isn't directly a problem\nsince we only ever perform TAR header file name validation on file reads, not\ndirectories, since the kernel VFS disallows reads on directories (`EISDIR`).\nCleaning the path, however, also removes the current working directory token\n(`./`) from a path. This means that if a path in a TAR file were prefixed with\n`./`, we would clean the path, removing the `./`, in turn causing our TAR header\nfile name validation check to fail when we attempt to read from the file.\nTo avoid TAR edge cases like this one, we should store TAR names as is in\nour metadata DB.\n\nSigned-off-by: Yasin Turan <turyasin@amazon.com>",
+          "timestamp": "2023-12-07T11:41:06-05:00",
+          "tree_id": "568172f3946feb88fd9610a67f0f34b4806247a5",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/cf25e82c4f729232981c5146890055da518007b3"
+        },
+        "date": 1701967868883,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 1.314,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0245,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.182,
             "unit": "Seconds",
             "extra": "P90"
           }
