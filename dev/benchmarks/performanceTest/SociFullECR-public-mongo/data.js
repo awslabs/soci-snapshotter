b34cd69d19f7330d4644da8885a0d4a472aa8cf4
github-action-benchmark <github@users.noreply.github.com>
2023-12-12 18:46:04 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 5d1022b0def534e457fd0f649b99e3874dc38eaa

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702330265808,
+  "lastUpdate": 1702406764108,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -2681,6 +2681,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.0115,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "5d1022b0def534e457fd0f649b99e3874dc38eaa",
+          "message": "Update Go version in CI to v1.20.12\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2023-12-12T10:34:32-08:00",
+          "tree_id": "4253100a6509502208864c5e41005b382f37c038",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/5d1022b0def534e457fd0f649b99e3874dc38eaa"
+        },
+        "date": 1702406760960,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 3.497,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.28700000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 0.9315,
             "unit": "Seconds",
             "extra": "P90"
           }
