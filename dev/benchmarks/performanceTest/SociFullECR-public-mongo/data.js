b20d43fe25d3c5b3d137620e905597985aeaacd1
github-action-benchmark <github@users.noreply.github.com>
2023-12-15 17:47:52 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 462303a7635a3df834e84a9cbcc15d3efa1b62bc

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702406764108,
+  "lastUpdate": 1702662472314,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -2723,6 +2723,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 0.9315,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "carlhilt@amazon.com",
+            "name": "Carl Hiltbrunner",
+            "username": "Subzidion"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "462303a7635a3df834e84a9cbcc15d3efa1b62bc",
+          "message": "Log successful startup of soci-snapshotter-grpc\n\nSigned-off-by: Carl Hiltbrunner <carlhilt@amazon.com>",
+          "timestamp": "2023-12-15T09:36:03-08:00",
+          "tree_id": "69b9c908e95cca8c2d02c1375ec74784a915484a",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/462303a7635a3df834e84a9cbcc15d3efa1b62bc"
+        },
+        "date": 1702662471776,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 9.995999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.296,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.5495,
             "unit": "Seconds",
             "extra": "P90"
           }
