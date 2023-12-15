2ba476fe4b19d82fcc02558b96e4b25c075a654b
github-action-benchmark <github@users.noreply.github.com>
2023-12-15 17:47:57 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 462303a7635a3df834e84a9cbcc15d3efa1b62bc

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702406757946,
+  "lastUpdate": 1702662477901,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -2641,6 +2641,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.971,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "carlhilt@amazon.com",
+            "name": "Carl Hiltbrunner",
+            "username": "Subzidion"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "462303a7635a3df834e84a9cbcc15d3efa1b62bc",
+          "message": "Log successful startup of soci-snapshotter-grpc\n\nSigned-off-by: Carl Hiltbrunner <carlhilt@amazon.com>",
+          "timestamp": "2023-12-15T09:36:03-08:00",
+          "tree_id": "69b9c908e95cca8c2d02c1375ec74784a915484a",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/462303a7635a3df834e84a9cbcc15d3efa1b62bc"
+        },
+        "date": 1702662473129,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 50.6725,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.2960000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 3.296,
             "unit": "Seconds",
             "extra": "P90"
           }
