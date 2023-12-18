7b9f84c9d0a8bc83fdd7d3d59363f7d777b2d248
github-action-benchmark <github@users.noreply.github.com>
2023-12-18 15:32:01 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 579268175a0fdde7c54ef14da0f57424d27b7dd8

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702913489645,
+  "lastUpdate": 1702913521827,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -2931,6 +2931,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.796,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "579268175a0fdde7c54ef14da0f57424d27b7dd8",
+          "message": "Remove test dependency on docker-compose\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2023-12-18T07:23:33-08:00",
+          "tree_id": "9a83a3257e40a625e3aa8c0cd16f0254772fd1a0",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/579268175a0fdde7c54ef14da0f57424d27b7dd8"
+        },
+        "date": 1702913521175,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 16.834000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.3015,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.8165,
             "unit": "Seconds",
             "extra": "P90"
           }
