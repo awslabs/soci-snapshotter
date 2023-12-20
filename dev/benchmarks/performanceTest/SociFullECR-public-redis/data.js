69a32bd5c7d70e83a9b937d1129ad8572f61eb5a
github-action-benchmark <github@users.noreply.github.com>
2023-12-20 21:49:08 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 680cbba65e8e81ceac04284698cc32ab583f480e

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1703095459683,
+  "lastUpdate": 1703108948068,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -3015,6 +3015,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.5999999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "680cbba65e8e81ceac04284698cc32ab583f480e",
+          "message": "Add documentation for most TOML variables\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2023-12-20T13:39:08-08:00",
+          "tree_id": "0ebcae466efd28c0a6d3bdada5942101c6fad9ae",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/680cbba65e8e81ceac04284698cc32ab583f480e"
+        },
+        "date": 1703108946793,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 2.326,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.020999999999999998,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.474,
             "unit": "Seconds",
             "extra": "P90"
           }
