f85579154ab4bc0ea06cc6fcd868ec3a53187fbb
github-action-benchmark <github@users.noreply.github.com>
2023-12-25 07:49:06 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 91ce7a8fcb74deb01a950b891b7c8ad5d0dcb2e1

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1703204322639,
+  "lastUpdate": 1703490546100,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -3142,6 +3142,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.6865,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anhtt109@vpbank.com.vn",
+            "name": "Tuan Anh Tran"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "91ce7a8fcb74deb01a950b891b7c8ad5d0dcb2e1",
+          "message": "fix: fix: strip path in release tar\n\nSigned-off-by: Tuan Anh Tran <anhtt109@vpbank.com.vn>",
+          "timestamp": "2023-12-24T23:39:32-08:00",
+          "tree_id": "b588108a93af66d065e4a1fc252b7b9c93e4f093",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/91ce7a8fcb74deb01a950b891b7c8ad5d0dcb2e1"
+        },
+        "date": 1703490543445,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.133,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.0155,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.8199999999999998,
             "unit": "Seconds",
             "extra": "P90"
           }
