0fcea1acb45b2459dc252296d874d9af3c075244
github-action-benchmark <github@users.noreply.github.com>
2024-01-17 21:14:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 335515f746f50c964ed48159257e1aeba04805b6

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1705435981179,
+  "lastUpdate": 1705526062774,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -3730,6 +3730,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.387,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davanum@gmail.com",
+            "name": "Davanum Srinivas",
+            "username": "dims"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "335515f746f50c964ed48159257e1aeba04805b6",
+          "message": "Leave Debug breadcrumbs for snapshotter functions called\n\nSigned-off-by: Davanum Srinivas <davanum@gmail.com>",
+          "timestamp": "2024-01-17T16:05:55-05:00",
+          "tree_id": "d14ab6a942093823a58e7c3a4b59a32a491cbfac",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/335515f746f50c964ed48159257e1aeba04805b6"
+        },
+        "date": 1705526061797,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 4.5065,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3155,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.4609999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
