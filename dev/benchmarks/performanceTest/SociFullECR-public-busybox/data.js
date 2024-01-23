bf716c28a33f55819da6e3d8cf4fad171e23d872
github-action-benchmark <github@users.noreply.github.com>
2024-01-23 19:17:13 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for db7df3ab2a840bc927417a84448263885b3e21ff

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706035012263,
+  "lastUpdate": 1706037433757,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -3939,6 +3939,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.1179999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "db7df3ab2a840bc927417a84448263885b3e21ff",
+          "message": "Fix file descriptor leak\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-01-23T11:07:22-08:00",
+          "tree_id": "4bb28524fbd14033535eaa41d6d75e165576cd76",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/db7df3ab2a840bc927417a84448263885b3e21ff"
+        },
+        "date": 1706037433135,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0315,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.8505000000000003,
             "unit": "Seconds",
             "extra": "P90"
           }
