d0d60c381fad91ef46888f3201c2c805f16fed0e
github-action-benchmark <github@users.noreply.github.com>
2024-02-02 23:32:02 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 04a83aaa56243152a0c14828a12cb810997ea6d8

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706847396933,
+  "lastUpdate": 1706916722274,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -4023,6 +4023,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.233,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "04a83aaa56243152a0c14828a12cb810997ea6d8",
+          "message": "Modify artifact verification script\n\nAllowed artifact verification to be called without having to cd into\nthe release directory\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-02-02T15:24:48-08:00",
+          "tree_id": "cd20bdccb2420224875483d34a685875ac332b02",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/04a83aaa56243152a0c14828a12cb810997ea6d8"
+        },
+        "date": 1706916720965,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0165,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.004,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 0.737,
             "unit": "Seconds",
             "extra": "P90"
           }
