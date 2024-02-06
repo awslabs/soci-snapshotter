4a8dcdc5b72b2ebd5c53d3782f4ca647af1cb65f
github-action-benchmark <github@users.noreply.github.com>
2024-02-06 21:31:23 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f5333b01e5ca34fa8221f79a8a116e014e51ce08

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706916725397,
+  "lastUpdate": 1707255083734,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -3901,6 +3901,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 0.8039999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "f5333b01e5ca34fa8221f79a8a116e014e51ce08",
+          "message": "Bump dependencies using scripts/bump-deps.sh\n\nSigned-off-by: GitHub <noreply@github.com>",
+          "timestamp": "2024-02-06T15:20:58-06:00",
+          "tree_id": "5cd3e2bb37e7ecaf2ad60e8f666db15103cf01ef",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f5333b01e5ca34fa8221f79a8a116e014e51ce08"
+        },
+        "date": 1707255078570,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 14.424,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 7.246,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 1.4395,
             "unit": "Seconds",
             "extra": "P90"
           }
