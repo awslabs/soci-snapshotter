414bdb504bb882f682febaa1f992667d6e5e0ae5
github-action-benchmark <github@users.noreply.github.com>
2024-02-06 21:31:17 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f5333b01e5ca34fa8221f79a8a116e014e51ce08

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706916724024,
+  "lastUpdate": 1707255077233,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -4065,6 +4065,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 0.9870000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "f5333b01e5ca34fa8221f79a8a116e014e51ce08",
+          "message": "Bump dependencies using scripts/bump-deps.sh\n\nSigned-off-by: GitHub <noreply@github.com>",
+          "timestamp": "2024-02-06T15:20:58-06:00",
+          "tree_id": "5cd3e2bb37e7ecaf2ad60e8f666db15103cf01ef",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f5333b01e5ca34fa8221f79a8a116e014e51ce08"
+        },
+        "date": 1707255076584,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.371,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.0085,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.4264999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
