99df9c0380ec630c293c7a63023dfdd91a5e7391
github-action-benchmark <github@users.noreply.github.com>
2024-02-27 15:11:40 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 3cd8cd448a09b4f9d69213da3075b3d045fd6a73

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708990552389,
+  "lastUpdate": 1709046700843,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -4317,6 +4317,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.1865,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "turyasin@amazon.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "3cd8cd448a09b4f9d69213da3075b3d045fd6a73",
+          "message": "Ensure bbolt KV pairs are inserted in key sorted order\n\nSigned-off-by: Yasin Turan <turyasin@amazon.com>",
+          "timestamp": "2024-02-27T10:04:12-05:00",
+          "tree_id": "8e93e925895b910d945fcfa0c0090ae2aeb0c7f6",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/3cd8cd448a09b4f9d69213da3075b3d045fd6a73"
+        },
+        "date": 1709046696768,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.138,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.007,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.5230000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
