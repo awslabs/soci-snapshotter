63da997fb5885d54d48ba020c6055eb74ab8180b
github-action-benchmark <github@users.noreply.github.com>
2024-02-27 15:11:38 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 3cd8cd448a09b4f9d69213da3075b3d045fd6a73

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708990559869,
+  "lastUpdate": 1709046698911,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -4194,6 +4194,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 1.6384999999999998,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "turyasin@amazon.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "3cd8cd448a09b4f9d69213da3075b3d045fd6a73",
+          "message": "Ensure bbolt KV pairs are inserted in key sorted order\n\nSigned-off-by: Yasin Turan <turyasin@amazon.com>",
+          "timestamp": "2024-02-27T10:04:12-05:00",
+          "tree_id": "8e93e925895b910d945fcfa0c0090ae2aeb0c7f6",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/3cd8cd448a09b4f9d69213da3075b3d045fd6a73"
+        },
+        "date": 1709046697970,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 11.692,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.4175,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.8795,
             "unit": "Seconds",
             "extra": "P90"
           }
