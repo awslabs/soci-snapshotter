36ed51809b0394eb3f79a0aea1721cd4efb5748c
github-action-benchmark <github@users.noreply.github.com>
2024-03-04 19:36:02 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for bb526c30097754683e91abb7836252b1bc3663f4

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709577528089,
+  "lastUpdate": 1709580962083,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -4527,6 +4527,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 2.1,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "bb526c30097754683e91abb7836252b1bc3663f4",
+          "message": "Update RUNC_VERSION in Dockerfile\n\nThis addresses CVE-2024-21626 in our testing suite. Note that our\nbinaries do not depend on runc, and thus are unaffected.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-03-04T11:25:22-08:00",
+          "tree_id": "ca222f98a34c20558e258320bbd84fe178756ae1",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/bb526c30097754683e91abb7836252b1bc3663f4"
+        },
+        "date": 1709580959566,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1965,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.0145,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 2.021,
             "unit": "Seconds",
             "extra": "P90"
           }
