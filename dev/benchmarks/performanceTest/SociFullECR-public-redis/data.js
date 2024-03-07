9cadde253c92704fc1553a065334c40047dd8a09
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 19:12:51 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 1d4d288d008d5f9b67f7179e25e2b6f22e8d1365

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709663478877,
+  "lastUpdate": 1709838771554,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -4652,6 +4652,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 0.7195,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "1d4d288d008d5f9b67f7179e25e2b6f22e8d1365",
+          "message": "Update zot image tag to v2.0.1\n\nThe v2.0.0-rc6 tag got removed, so using the latest stable version\nas of the time of this commit.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-03-07T11:02:45-08:00",
+          "tree_id": "f0d00c483ca3a8d05c9f116856f02f010cdd3ebf",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/1d4d288d008d5f9b67f7179e25e2b6f22e8d1365"
+        },
+        "date": 1709838770940,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 3.012,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.02,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.713,
             "unit": "Seconds",
             "extra": "P90"
           }
