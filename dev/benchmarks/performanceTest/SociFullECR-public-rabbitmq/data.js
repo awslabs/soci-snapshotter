d59ca73f62373b55188e670229c91084f2c002c1
github-action-benchmark <github@users.noreply.github.com>
2024-03-15 17:50:51 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 27d6b4064e76871646fe021258556de436b669ae

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710428993037,
+  "lastUpdate": 1710525051804,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -4531,6 +4531,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.8315,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "27d6b4064e76871646fe021258556de436b669ae",
+          "message": "Update Golang to v1.21.7\n\nDockerfile + workflows — 1.20.13 -> 1.21.7\ngo.mod — 1.20 -> 1.21\ngolangci-lint — 1.53.3 -> 1.56.2\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-03-15T10:42:37-07:00",
+          "tree_id": "4e2567a4e86cd5fe989120713488e14e88401ddc",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/27d6b4064e76871646fe021258556de436b669ae"
+        },
+        "date": 1710525044895,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 8.734,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 7.4295,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 0.7865,
             "unit": "Seconds",
             "extra": "P90"
           }
