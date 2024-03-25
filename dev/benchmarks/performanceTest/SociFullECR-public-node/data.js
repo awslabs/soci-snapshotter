7aec57bba45f1686d7d9b5aa8e733d256e456514
github-action-benchmark <github@users.noreply.github.com>
2024-03-25 22:18:57 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 9d6a4202b4831daa193ccd08ae125ba216b09780

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711134806250,
+  "lastUpdate": 1711405137767,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -4992,6 +4992,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.4595,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "9d6a4202b4831daa193ccd08ae125ba216b09780",
+          "message": "Add shellcheck to lint\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-03-25T15:10:56-07:00",
+          "tree_id": "251be8188e5ba5ee274b7279eefcf25c49ba8b34",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/9d6a4202b4831daa193ccd08ae125ba216b09780"
+        },
+        "date": 1711405137126,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 6.271000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4395,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.3275000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
