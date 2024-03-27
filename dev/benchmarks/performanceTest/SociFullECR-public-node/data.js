8173fa4070c3797f94d9cf7e46d1f0bb05413a1b
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 19:24:41 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f78090a12c74ec4a98c9172d519a8811dbf8d97c

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711559514187,
+  "lastUpdate": 1711567481201,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -5286,6 +5286,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.246,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun Raja Yogidas",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "wh_henry@hotmail.com",
+            "name": "Henry Wang",
+            "username": "henry118"
+          },
+          "distinct": true,
+          "id": "f78090a12c74ec4a98c9172d519a8811dbf8d97c",
+          "message": "Update containerd version in build.yml\n\nSigned-off-by: Arjun Raja <arjunry@amazon.com>",
+          "timestamp": "2024-03-27T12:14:52-07:00",
+          "tree_id": "3560b08a303f2a8042018ef622e1d09ac7591182",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f78090a12c74ec4a98c9172d519a8811dbf8d97c"
+        },
+        "date": 1711567478811,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 6.5345,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4345,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.1965,
             "unit": "Seconds",
             "extra": "P90"
           }
