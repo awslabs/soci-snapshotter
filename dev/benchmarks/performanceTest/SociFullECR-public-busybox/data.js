1b324086d7cb4f12944ed6cd1d654e30147408aa
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 22:52:15 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for e8ffbc8f7f93e3313f30af2650ff9ba251c666ba

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711567487243,
+  "lastUpdate": 1711579935787,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -5451,6 +5451,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.3835000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "wh_henry@hotmail.com",
+            "name": "Henry Wang",
+            "username": "henry118"
+          },
+          "distinct": true,
+          "id": "e8ffbc8f7f93e3313f30af2650ff9ba251c666ba",
+          "message": "Update registry test dependency to registry:3.0.0-alpha.1\n\nThis change updates the test registry version to v3.0.0-alpha.1 for\nregistry CVE updates.\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-03-27T15:44:43-07:00",
+          "tree_id": "688fa07f8e7e7da10eb202e3d755ad0eb6842c63",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/e8ffbc8f7f93e3313f30af2650ff9ba251c666ba"
+        },
+        "date": 1711579931515,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.019,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.004,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.3295,
             "unit": "Seconds",
             "extra": "P90"
           }
