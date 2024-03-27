caf235802692d88a0cbc22223b5cfc8277e1abd9
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 23:02:34 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 373be7f0fa8cb9ba631c55a45fb8c61eacbdecc8

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711579937154,
+  "lastUpdate": 1711580554215,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -5452,6 +5452,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.464,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "373be7f0fa8cb9ba631c55a45fb8c61eacbdecc8",
+          "message": "Run shellcheck from container in CI\n\nThis change runs shellcheck from container in CI instead of installing\nthe binary to the system and running from the lint bash script.\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-03-27T15:52:20-07:00",
+          "tree_id": "745cfd2435e374186ae326719449c18e8d0c4f65",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/373be7f0fa8cb9ba631c55a45fb8c61eacbdecc8"
+        },
+        "date": 1711580553387,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.7075,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3085,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.4140000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
