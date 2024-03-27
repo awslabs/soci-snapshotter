9ac423bcb030e89b4c6d7efdf1bcd726d9e31c16
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 17:11:51 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 41bbfbb384eaad104bb8b7393d0b8e2cf5e60dc1

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711559340585,
+  "lastUpdate": 1711559511664,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -5367,6 +5367,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.2255,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "wh_henry@hotmail.com",
+            "name": "Henry Wang",
+            "username": "henry118"
+          },
+          "distinct": true,
+          "id": "41bbfbb384eaad104bb8b7393d0b8e2cf5e60dc1",
+          "message": "Add debug logging to metadata DB initialization\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-03-27T10:03:22-07:00",
+          "tree_id": "6189f24d7ce6a0b6b80c80df461fdcf72f85f4ba",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/41bbfbb384eaad104bb8b7393d0b8e2cf5e60dc1"
+        },
+        "date": 1711559506465,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.296,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.949,
             "unit": "Seconds",
             "extra": "P90"
           }
