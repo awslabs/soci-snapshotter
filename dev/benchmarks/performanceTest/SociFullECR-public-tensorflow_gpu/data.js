52320505ae6e7686eb5259e3c6f18a821360f00b
github-action-benchmark <github@users.noreply.github.com>
2024-04-02 18:41:34 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d0e78441b1a03e88fe5d74fd875c8df5968a0b68

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712078878695,
+  "lastUpdate": 1712083294761,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -5622,6 +5622,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 1.7605,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "d0e78441b1a03e88fe5d74fd875c8df5968a0b68",
+          "message": "Pull container images from ECR Public instead of Docker Hub\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-04-02T11:33:06-07:00",
+          "tree_id": "58f7791573dcd0457e1df33050bdc490f42370c3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d0e78441b1a03e88fe5d74fd875c8df5968a0b68"
+        },
+        "date": 1712083293984,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 15.488500000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.48,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.56,
             "unit": "Seconds",
             "extra": "P90"
           }
