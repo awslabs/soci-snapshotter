d94362d295a272ef3e9bcb934dadb5dde4f07f49
github-action-benchmark <github@users.noreply.github.com>
2024-04-09 17:28:46 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for dbdef0840b1d5fa609273829f438361d5a5d9f70

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712181290672,
+  "lastUpdate": 1712683726336,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -5790,6 +5790,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.083,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "dbdef0840b1d5fa609273829f438361d5a5d9f70",
+          "message": "Address yamllint findings\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-04-09T10:19:41-07:00",
+          "tree_id": "8bbd5cc118003574234ff8da2e180a67f5dedb9a",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/dbdef0840b1d5fa609273829f438361d5a5d9f70"
+        },
+        "date": 1712683721077,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 6.298,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4385,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.6030000000000002,
             "unit": "Seconds",
             "extra": "P90"
           }
