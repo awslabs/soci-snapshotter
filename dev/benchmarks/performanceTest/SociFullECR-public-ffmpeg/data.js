b8a7a174aa64eb66c991e6195a2a306c8c39b04b
github-action-benchmark <github@users.noreply.github.com>
2024-04-11 22:42:18 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 779edee7a038c13875945f4c6db56c575d0c0266

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712875110095,
+  "lastUpdate": 1712875338127,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -6039,6 +6039,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 2.0455,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": true,
+          "id": "779edee7a038c13875945f4c6db56c575d0c0266",
+          "message": "Disable TestNetworkRetry on ARM machines\n\nThis test currently hangs on ARM machines, so disabling till we can fix\nthis.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-04-11T15:33:37-07:00",
+          "tree_id": "5bea95a4da1476f3ef79c504292734e370aff90c",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/779edee7a038c13875945f4c6db56c575d0c0266"
+        },
+        "date": 1712875337348,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1915,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.0095,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 2.043,
             "unit": "Seconds",
             "extra": "P90"
           }
