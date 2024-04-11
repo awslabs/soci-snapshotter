1870e364469641e985482ad5f422e6c462fb6aa4
github-action-benchmark <github@users.noreply.github.com>
2024-04-11 22:38:26 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for afec192851a0783ed504d49f06997aa57c7d01c4

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712683847780,
+  "lastUpdate": 1712875106232,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -5997,6 +5997,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.419,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "afec192851a0783ed504d49f06997aa57c7d01c4",
+          "message": "Check first header in each layer\n\nIn #1147, we cherry-picked a commit from stargz where we don't make\nregistry calls if layers are fully pulled. This works fine for stargz,\nbut for SOCI, we skip reading the first header for each layer, as we\ndon't need it. This caused a bug where our fetched size never matched\nour expected size, so this condition was never met.\n\nThis commit fixes this by reading the initially skipped header. This\ncommit also checks this header to ensure that it is not malformed for\nany reason.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-04-11T15:29:53-07:00",
+          "tree_id": "cf0bbd527eb9906bbf8fc3692e94f198c09180c8",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/afec192851a0783ed504d49f06997aa57c7d01c4"
+        },
+        "date": 1712875105483,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0315,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.0045000000000000005,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.3445,
             "unit": "Seconds",
             "extra": "P90"
           }
