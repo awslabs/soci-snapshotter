9a7ccb32e2440574d150c92c4ac813266223cd61
github-action-benchmark <github@users.noreply.github.com>
2024-05-02 06:04:14 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for bf4c69d854d8e10be4a555658010277139d6ff12

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714605821305,
+  "lastUpdate": 1714629854022,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -6458,6 +6458,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.4685000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "bf4c69d854d8e10be4a555658010277139d6ff12",
+          "message": "Use public ECR zot image\n\nPer Amazon best security practices, we switched to using a version of\nproject zot hosted on public ECR instead of ghcr.io.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-05-01T22:56:23-07:00",
+          "tree_id": "6cb04fd54212aad678777ed085074bd04cd86181",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/bf4c69d854d8e10be4a555658010277139d6ff12"
+        },
+        "date": 1714629853256,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 9.557500000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.29,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.4024999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
