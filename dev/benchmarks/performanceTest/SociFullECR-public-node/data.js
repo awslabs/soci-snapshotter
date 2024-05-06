8ae79be67f42173ecc010c3c49a917c2f89edbc6
github-action-benchmark <github@users.noreply.github.com>
2024-05-06 19:53:36 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 2cb31a4f8277a31a29d4bb3c441231647a2b5177

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714629858441,
+  "lastUpdate": 1715025216657,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -6336,6 +6336,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.3615,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "2cb31a4f8277a31a29d4bb3c441231647a2b5177",
+          "message": "Add timeouts to testing in CI\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-05-06T15:44:11-04:00",
+          "tree_id": "5a4913faa4197550211a17c39274852418e41297",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/2cb31a4f8277a31a29d4bb3c441231647a2b5177"
+        },
+        "date": 1715025210504,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 5.6275,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.438,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.5575,
             "unit": "Seconds",
             "extra": "P90"
           }
