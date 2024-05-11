915037b152d2fffee316c715c093271cfe8128b7
github-action-benchmark <github@users.noreply.github.com>
2024-05-11 11:51:21 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 071a9111ec94afedd38f4deac7c960b803710059

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715428140891,
+  "lastUpdate": 1715428281031,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -6878,6 +6878,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 0.9135,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "steven.davidovitz@dominodatalab.com",
+            "name": "Steven Davidovitz",
+            "username": "steved"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "071a9111ec94afedd38f4deac7c960b803710059",
+          "message": "Trigger Docker auth on ECR token expiry\n\nAn expired token passed to ECR will return a 403, which lacks a\nWww-Authenticate header required to trigger the docker authorizer. This\nmeant that credential helpers like amazon-ecr-credential-helper would\nnot refresh the token. This change adds the proper header to fix this\nbehavior.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-05-11T04:42:39-07:00",
+          "tree_id": "5084428f0af5e11612e6aaacf4d58e3bb790124b",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/071a9111ec94afedd38f4deac7c960b803710059"
+        },
+        "date": 1715428280348,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 0.6765,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0225,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 2.7255000000000003,
             "unit": "Seconds",
             "extra": "P90"
           }
