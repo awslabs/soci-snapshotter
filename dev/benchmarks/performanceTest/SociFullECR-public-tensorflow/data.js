fcfdf212cf6c011595b1d4f8f1cf77bf44afeb6c
github-action-benchmark <github@users.noreply.github.com>
2024-05-13 16:56:49 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 292499059e1bc84bef2fa227b7c56361752cb7cf

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715428284675,
+  "lastUpdate": 1715619409669,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -6920,6 +6920,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 3.3795,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "292499059e1bc84bef2fa227b7c56361752cb7cf",
+          "message": "Regenerate changed flatbuffer definitions\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-05-13T09:49:29-07:00",
+          "tree_id": "955bfde5fbd2a27cea3d41d2cb196fbc40242545",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/292499059e1bc84bef2fa227b7c56361752cb7cf"
+        },
+        "date": 1715619404020,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 9.2065,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.1959999999999997,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.478,
             "unit": "Seconds",
             "extra": "P90"
           }
