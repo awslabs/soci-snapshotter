d392c738fa3ded174701c60b3fd1b5fe98f60358
Austin Vazquez <macedonv@amazon.com>
2024-05-23 19:45:46 +0000

    Add go licenses check to CI
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

--- scripts/check-third-party-licenses.sh
+++ scripts/check-third-party-licenses.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
 # may not use this file except in compliance with the License. A copy of
@@ -18,19 +18,24 @@ set -euo pipefail
 # Normalize to working directory being root (up one level from ./scripts)
 root=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 
-pushd "${root}/ecr-login"
+pushd "${root}"
 
 # Fail third party dependency usage if not covered by the curated set of pre-approved licenses.
 #
 # List was generated from guidance set forth by Amazon open source usage policies.
 #
+# The SOCI project, with the exception of its usage of MPL-2.0, additionally follows the guidance
+# set forth by the CNCF Allowlist License Policy.
+#
+# https://github.com/cncf/foundation/blob/88f1a47550eb2df71b4b6e9c148a1c2f99a1d92e/allowed-third-party-license-policy.md
+#
 # Additional usage of third party dependencies not covered by the following licenses
 # will need maintainer approval in alignment with Amazon open source usage policies.
 #
-# Requests can be made via https://github.com/awslabs/amazon-ecr-credential-helper/issues/new/choose
+# Requests can be made via https://github.com/awslabs/soci-snapshotter/issues/new/choose
 go-licenses check \
     --include_tests \
-    --ignore github.com/awslabs/amazon-ecr-credential-helper \
-    --allowed_licenses=Apache-2.0,BSD-3-Clause,MIT,ISC, ./...
+    --ignore github.com/awslabs/soci-snapshotter \
+    --allowed_licenses=Apache-2.0,BSD-2-Clause,BSD-2-Clause-FreeBSD,BSD-3-Clause,MIT,ISC,Python-2.0,PostgreSQL,X11,Zlib,MPL-2.0 ./...
 
 popd
