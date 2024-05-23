4a973809db83b58e4911cc66504d2dc7d277ad65
github-action-benchmark <github@users.noreply.github.com>
2024-05-23 20:09:06 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 81b170fdf88c19efa3c35c431abd3cfd511c6903

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716475803400,
+  "lastUpdate": 1716494946016,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7089,6 +7089,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 0.455,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "81b170fdf88c19efa3c35c431abd3cfd511c6903",
+          "message": "Bump docker/library/golang from 1.21.9-alpine to 1.21.10-alpine\n\nBumps docker/library/golang from 1.21.9-alpine to 1.21.10-alpine.\n\n---\nupdated-dependencies:\n- dependency-name: docker/library/golang\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-05-23T12:59:49-07:00",
+          "tree_id": "b6767dc2f1661eb0fbc004fb61b6cd99f2f5c7b0",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/81b170fdf88c19efa3c35c431abd3cfd511c6903"
+        },
+        "date": 1716494944777,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.021500000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.004,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 0.7669999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
