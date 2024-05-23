77b73361ce695c5975f86d33be42220f7a574bd1
github-action-benchmark <github@users.noreply.github.com>
2024-05-23 14:50:04 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d0a67cf8230d2b9a4ffd136a770f200de6057aae

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716403271490,
+  "lastUpdate": 1716475804963,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -6883,6 +6883,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.666,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "d0a67cf8230d2b9a4ffd136a770f200de6057aae",
+          "message": "Add dependabot configuration to ignore Go minor updates\n\nThis change adds dependabot configuration to ignore Go major and minor\nupdates. This is to keep the container image aligned with the version\nused by the release pipeline. A major update should not occur.\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-05-23T07:43:10-07:00",
+          "tree_id": "92d6967acaec2093eed70bfbdf112667bd1d09fc",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d0a67cf8230d2b9a4ffd136a770f200de6057aae"
+        },
+        "date": 1716475798349,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 8.3845,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 7.2844999999999995,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 0.669,
             "unit": "Seconds",
             "extra": "P90"
           }
