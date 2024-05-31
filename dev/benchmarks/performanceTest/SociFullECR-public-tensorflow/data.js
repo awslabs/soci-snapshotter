4523763b8159f95f1ddfe082069cdfe54a43e8dd
github-action-benchmark <github@users.noreply.github.com>
2024-05-31 17:52:56 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 310a88e7a7d9ef8fcc6f8c835edb2edb71a87642

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716494949873,
+  "lastUpdate": 1717177976790,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7172,6 +7172,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.991,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "310a88e7a7d9ef8fcc6f8c835edb2edb71a87642",
+          "message": "Add workflow to update getting started guide\n\nThis change adds an automated workflow to open a pull request to update the\nversion in the getting started guide when a release is made.\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-05-31T10:44:25-07:00",
+          "tree_id": "efa4a8b4b08318e0a9c1163f145b8dcf9d959567",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/310a88e7a7d9ef8fcc6f8c835edb2edb71a87642"
+        },
+        "date": 1717177975407,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 28.802,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.225,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.8615,
             "unit": "Seconds",
             "extra": "P90"
           }
