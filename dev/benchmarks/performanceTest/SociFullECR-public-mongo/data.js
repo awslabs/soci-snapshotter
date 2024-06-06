bf9c1de4145249a72b8c73d15ebd81c4b9f60fd1
github-action-benchmark <github@users.noreply.github.com>
2024-06-06 19:32:32 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for c538952debec5e42451b5accf04dc77ac60a7558

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717435696733,
+  "lastUpdate": 1717702352219,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7216,6 +7216,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.182,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sbora@amazon.com",
+            "name": "Swagat Bora",
+            "username": "swagatbora90"
+          },
+          "committer": {
+            "email": "swagatbora90@gmail.com",
+            "name": "Swagat Bora",
+            "username": "swagatbora90"
+          },
+          "distinct": true,
+          "id": "c538952debec5e42451b5accf04dc77ac60a7558",
+          "message": "Update Go version to 1.21.11 in CI\n\nSigned-off-by: Swagat Bora <sbora@amazon.com>",
+          "timestamp": "2024-06-06T12:22:31-07:00",
+          "tree_id": "ed9d4cd916a88d0186a88301f963f8a42e6eb3e8",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/c538952debec5e42451b5accf04dc77ac60a7558"
+        },
+        "date": 1717702346559,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 7.992000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3115,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.776,
             "unit": "Seconds",
             "extra": "P90"
           }
