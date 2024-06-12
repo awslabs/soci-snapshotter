6306dbfd804f0eaf758945821e68524cff14148a
github-action-benchmark <github@users.noreply.github.com>
2024-06-12 23:41:36 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 184d564b9003d0fca62b6e8cc331bb5451a06bc3

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717710097428,
+  "lastUpdate": 1718235696301,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7260,6 +7260,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.9569999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "184d564b9003d0fca62b6e8cc331bb5451a06bc3",
+          "message": "Add workflow to label pull requests\n\nThis change adds a GitHub Actions workflow to label PR's based on their\nchanges. The following labels will be applied to pull requests:\n\n- `documentation` - updates to `**/*.md`\n- `go` - updates to `**/*.go`\n- `testing` - updates to `**/*_test.go`\n- `github_actions` - updates to `.github/`\n- `benchmarking` - updates to `benchmark/`\n- `dependencies` - updates to `go.(mod|sum)`\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-06-12T16:32:52-07:00",
+          "tree_id": "f719eade37bc161b09ccdd52d683701e38a5901d",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/184d564b9003d0fca62b6e8cc331bb5451a06bc3"
+        },
+        "date": 1718235695537,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 3.125,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4475,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.212,
             "unit": "Seconds",
             "extra": "P90"
           }
