769cf4d0d54f46bd00ebbbcc2bb46475e10ee490
github-action-benchmark <github@users.noreply.github.com>
2024-06-19 22:55:46 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 07296f9741ff540f226125dfa795270c2f9900a7

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718740532791,
+  "lastUpdate": 1718837746281,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7428,6 +7428,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.7295,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shubhum@amazon.com",
+            "name": "Shubharanshu Mahapatra",
+            "username": "Shubhranshu153"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "07296f9741ff540f226125dfa795270c2f9900a7",
+          "message": "Fix daemon startup latency issue by providing an option to skip snapshotter supported flag\n\nSigned-off-by: Shubharanshu Mahapatra <shubhum@amazon.com>",
+          "timestamp": "2024-06-19T15:48:43-07:00",
+          "tree_id": "3b0ccddec373fb6aec21c3e932067bbfe0bb32a9",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/07296f9741ff540f226125dfa795270c2f9900a7"
+        },
+        "date": 1718837745259,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 9.682,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.2664999999999997,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 1.8900000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
