8ba4f8ce1a8996ef3ccef1b05fbef8f5ad1bde4d
github-action-benchmark <github@users.noreply.github.com>
2024-06-19 22:55:53 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 07296f9741ff540f226125dfa795270c2f9900a7

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718740530462,
+  "lastUpdate": 1718837753090,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7551,6 +7551,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.1555,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shubhum@amazon.com",
+            "name": "Shubharanshu Mahapatra",
+            "username": "Shubhranshu153"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "07296f9741ff540f226125dfa795270c2f9900a7",
+          "message": "Fix daemon startup latency issue by providing an option to skip snapshotter supported flag\n\nSigned-off-by: Shubharanshu Mahapatra <shubhum@amazon.com>",
+          "timestamp": "2024-06-19T15:48:43-07:00",
+          "tree_id": "3b0ccddec373fb6aec21c3e932067bbfe0bb32a9",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/07296f9741ff540f226125dfa795270c2f9900a7"
+        },
+        "date": 1718837748144,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.009999999999999998,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.004,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.0435,
             "unit": "Seconds",
             "extra": "P90"
           }
