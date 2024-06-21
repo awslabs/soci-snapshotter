82c5b9d62f1eb2f6206a22db69bdff7a827143bd
github-action-benchmark <github@users.noreply.github.com>
2024-06-21 16:04:25 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 145a4382bad97921bb72b08a8376d6276b58afbc

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718837749008,
+  "lastUpdate": 1718985865871,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7470,6 +7470,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.6745,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "swagatbora90@gmail.com",
+            "name": "Swagat Bora",
+            "username": "swagatbora90"
+          },
+          "distinct": true,
+          "id": "145a4382bad97921bb72b08a8376d6276b58afbc",
+          "message": "Auto-update packages in Dockerfile\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-06-21T08:56:25-07:00",
+          "tree_id": "53fd21ddcb40740f3af3c8d794af8a8e7170981d",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/145a4382bad97921bb72b08a8376d6276b58afbc"
+        },
+        "date": 1718985863960,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 3.7805,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.459,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.9095,
             "unit": "Seconds",
             "extra": "P90"
           }
