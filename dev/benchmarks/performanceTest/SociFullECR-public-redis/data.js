a8c9735ca16d234742bc8b1a4c8b7c26ecc4a680
github-action-benchmark <github@users.noreply.github.com>
2024-06-24 16:26:23 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for a857d2b83d3c86e70392ae658e918816c747d5d8

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718985860164,
+  "lastUpdate": 1719246383822,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7676,6 +7676,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.343,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shubhum@amazon.com",
+            "name": "Shubharanshu Mahapatra",
+            "username": "Shubhranshu153"
+          },
+          "committer": {
+            "email": "47723536+Shubhranshu153@users.noreply.github.com",
+            "name": "Subhransu",
+            "username": "Shubhranshu153"
+          },
+          "distinct": true,
+          "id": "a857d2b83d3c86e70392ae658e918816c747d5d8",
+          "message": "Bump version of containerd to 1.7.18 for latest and 1.6.33 for LTS\n\nSigned-off-by: Shubharanshu Mahapatra <shubhum@amazon.com>",
+          "timestamp": "2024-06-24T09:18:25-07:00",
+          "tree_id": "a2d8d2413c0a74defbfb63af7c7eec8e8f416f20",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/a857d2b83d3c86e70392ae658e918816c747d5d8"
+        },
+        "date": 1719246381005,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 1.476,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0215,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.3915,
             "unit": "Seconds",
             "extra": "P90"
           }
