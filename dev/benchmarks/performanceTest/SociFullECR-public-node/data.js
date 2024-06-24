655b79ffad2fd1e8d7aa49bbb71e96ed4b7e6a62
github-action-benchmark <github@users.noreply.github.com>
2024-06-24 16:26:26 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for a857d2b83d3c86e70392ae658e918816c747d5d8

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718985865871,
+  "lastUpdate": 1719246386176,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7512,6 +7512,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.9095,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shubhum@amazon.com",
+            "name": "Shubharanshu Mahapatra",
+            "username": "Shubhranshu153"
+          },
+          "committer": {
+            "email": "47723536+Shubhranshu153@users.noreply.github.com",
+            "name": "Subhransu",
+            "username": "Shubhranshu153"
+          },
+          "distinct": true,
+          "id": "a857d2b83d3c86e70392ae658e918816c747d5d8",
+          "message": "Bump version of containerd to 1.7.18 for latest and 1.6.33 for LTS\n\nSigned-off-by: Shubharanshu Mahapatra <shubhum@amazon.com>",
+          "timestamp": "2024-06-24T09:18:25-07:00",
+          "tree_id": "a2d8d2413c0a74defbfb63af7c7eec8e8f416f20",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/a857d2b83d3c86e70392ae658e918816c747d5d8"
+        },
+        "date": 1719246381762,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 1.972,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.453,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.7335,
             "unit": "Seconds",
             "extra": "P90"
           }
