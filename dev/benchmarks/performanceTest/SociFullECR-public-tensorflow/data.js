fc4bed2a364248cf599215fabdc43d3ff6cd7d11
github-action-benchmark <github@users.noreply.github.com>
2024-07-02 22:43:54 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 2f82461d214d2bc30843af32c52b7883b304db60

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719960016024,
+  "lastUpdate": 1719960234147,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7844,6 +7844,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.8665000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "2f82461d214d2bc30843af32c52b7883b304db60",
+          "message": "Allow insecure localhost connections\n\nBefore this change, the `docker.MatchLocalhost` function was applied to\nhosts retrieved from labels on snapshots, but not in the artifact\nfetcher. This meant that data could be lazily loaded from an insecure\nlocalhost, but we couldn't fetch the SOCI index/ztocs from an insecure\nlocalhost. This change adds the matcher to the artifact fetcher so that\nimages can be lazily loaded from an insecure localhost.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2024-07-02T15:32:40-07:00",
+          "tree_id": "7907d519c02959f738f4bfb449a3249cc69e6075",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/2f82461d214d2bc30843af32c52b7883b304db60"
+        },
+        "date": 1719960231818,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 37.5625,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.2815000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.9059999999999997,
             "unit": "Seconds",
             "extra": "P90"
           }
