a03472913d47065dd1eb14c8f0f1330612df6fb6
github-action-benchmark <github@users.noreply.github.com>
2024-07-02 22:43:52 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 2f82461d214d2bc30843af32c52b7883b304db60

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719960012120,
+  "lastUpdate": 1719960232605,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7803,6 +7803,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.177,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "2f82461d214d2bc30843af32c52b7883b304db60",
+          "message": "Allow insecure localhost connections\n\nBefore this change, the `docker.MatchLocalhost` function was applied to\nhosts retrieved from labels on snapshots, but not in the artifact\nfetcher. This meant that data could be lazily loaded from an insecure\nlocalhost, but we couldn't fetch the SOCI index/ztocs from an insecure\nlocalhost. This change adds the matcher to the artifact fetcher so that\nimages can be lazily loaded from an insecure localhost.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2024-07-02T15:32:40-07:00",
+          "tree_id": "7907d519c02959f738f4bfb449a3249cc69e6075",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/2f82461d214d2bc30843af32c52b7883b304db60"
+        },
+        "date": 1719960231128,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.022,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.0045000000000000005,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.556,
             "unit": "Seconds",
             "extra": "P90"
           }
