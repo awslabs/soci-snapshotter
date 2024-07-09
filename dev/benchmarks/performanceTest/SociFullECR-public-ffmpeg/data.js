352b3568da7177f22fb598669dcde305f0042967
github-action-benchmark <github@users.noreply.github.com>
2024-07-09 20:41:08 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 0d06c8814527d128499f08422e892e4ca0c83b24

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720198178906,
+  "lastUpdate": 1720557668741,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7887,6 +7887,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.5605,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "0d06c8814527d128499f08422e892e4ca0c83b24",
+          "message": "Update deprecated packages\n\ngithub.com/containerd/containerd/platforms ->\ngithub.com/containerd/platforms\n\ngithub.com/containerd/containerd/reference/docker ->\ngithub.com/distribution/reference\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-07-09T13:32:51-07:00",
+          "tree_id": "70f6ffe8aefa1cbe89d4f6ca651875a0632ea819",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/0d06c8814527d128499f08422e892e4ca0c83b24"
+        },
+        "date": 1720557665119,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.20950000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.008,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.207,
             "unit": "Seconds",
             "extra": "P90"
           }
