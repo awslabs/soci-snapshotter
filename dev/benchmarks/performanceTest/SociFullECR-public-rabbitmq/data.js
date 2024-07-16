e0689f9872078416ef1e16c918ae589e31fa2a26
github-action-benchmark <github@users.noreply.github.com>
2024-07-16 16:04:28 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 9eef2c0ff55f7e52d9b2f47364dce56eaef63e9e

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720579751888,
+  "lastUpdate": 1721145868533,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -7849,6 +7849,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.5445,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": true,
+          "id": "9eef2c0ff55f7e52d9b2f47364dce56eaef63e9e",
+          "message": "Use rabbitmqImage in FUSE failure test\n\nOn ARM instances, pinnedRabbitmqImage does not work for this test due to\nsome issues with xattr/whiteout dir detection. This is either a bug in\nthe image or our code, but to get around CI for now we will just use a\nregular rabbitmq image.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-07-16T08:54:51-07:00",
+          "tree_id": "17760429481a83641d9808691159a22144684d68",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/9eef2c0ff55f7e52d9b2f47364dce56eaef63e9e"
+        },
+        "date": 1721145865804,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 15.810500000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 8.567,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 1.733,
             "unit": "Seconds",
             "extra": "P90"
           }
