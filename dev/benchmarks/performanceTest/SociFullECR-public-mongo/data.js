a12e479800e696a90527843308dcb2a346836ef3
github-action-benchmark <github@users.noreply.github.com>
2024-07-17 22:00:15 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for a641e0de43b1e5c2939fc36f9c8727933d2da8de

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721232466913,
+  "lastUpdate": 1721253615057,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -8098,6 +8098,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.1435,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "a641e0de43b1e5c2939fc36f9c8727933d2da8de",
+          "message": "Revert \"Disable xattrs by default\"\n\nThis reverts commit 2a70d12d833f8e93f62dea30d400bac1e2d7810d and some\nextra reverts related to it.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-07-17T14:52:37-07:00",
+          "tree_id": "f158afe3c3e171afc1f9d4f85e3b74d23386dc16",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/a641e0de43b1e5c2939fc36f9c8727933d2da8de"
+        },
+        "date": 1721253612647,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.6885,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.326,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 0.67,
             "unit": "Seconds",
             "extra": "P90"
           }
