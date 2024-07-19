9d68608627b54c89d549827b2a1fa81c0fb638c8
github-action-benchmark <github@users.noreply.github.com>
2024-07-19 22:07:26 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 846397c346c81fc03b8eab44236c0dfe7f22419b

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721426839709,
+  "lastUpdate": 1721426846204,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -8143,6 +8143,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 0.7065,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "846397c346c81fc03b8eab44236c0dfe7f22419b",
+          "message": "Add integration test for no image index\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-07-19T14:59:41-07:00",
+          "tree_id": "6541eaf5b9b1dee3e1b59f711d9e9b288aa55760",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/846397c346c81fc03b8eab44236c0dfe7f22419b"
+        },
+        "date": 1721426843383,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 9.785499999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 8.725,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 0.6475,
             "unit": "Seconds",
             "extra": "P90"
           }
