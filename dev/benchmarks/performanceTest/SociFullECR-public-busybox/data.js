f45e174ab89683cdbfc3a4f2233933577a269e84
github-action-benchmark <github@users.noreply.github.com>
2024-08-02 17:59:28 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d0fc67eb4aaf7870afac4a607b053a718da8a9af

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722404777480,
+  "lastUpdate": 1722621568936,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -8391,6 +8391,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 0.7364999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "d0fc67eb4aaf7870afac4a607b053a718da8a9af",
+          "message": "Add review dependencies workflow to CI\n\nThis change adds a new CI workflow for reviewing added Go dependencies.\nThis include license and vulnerability checks.\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2024-08-02T10:49:15-07:00",
+          "tree_id": "9a2a2437d50a3648e6c52b6c78aa0adb5be0cc7e",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d0fc67eb4aaf7870afac4a607b053a718da8a9af"
+        },
+        "date": 1722621567405,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.035500000000000004,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.0095,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 0.8965,
             "unit": "Seconds",
             "extra": "P90"
           }
