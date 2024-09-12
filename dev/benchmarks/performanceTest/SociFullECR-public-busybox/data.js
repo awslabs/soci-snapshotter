1fd6e8ebacd822777aa0c68cbbe3281042843f14
github-action-benchmark <github@users.noreply.github.com>
2024-09-12 02:58:35 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 871bec22b92b8f343d3546f76c76826fa526aab1

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725897691317,
+  "lastUpdate": 1726109915393,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -8853,6 +8853,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 0.8029999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "871bec22b92b8f343d3546f76c76826fa526aab1",
+          "message": "Add test coverage to workflows\n\nThis change adds code coverage to our workflows and the necessary\nMakefile targets to support this.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-09-11T19:49:27-07:00",
+          "tree_id": "e46d04ff2bba5287f537efe72aabaa078d9b6d50",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/871bec22b92b8f343d3546f76c76826fa526aab1"
+        },
+        "date": 1726109910326,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.013000000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.0095,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.291,
             "unit": "Seconds",
             "extra": "P90"
           }
