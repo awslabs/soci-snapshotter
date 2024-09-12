242316ef1f0a7c772eea529de2b17228084837df
github-action-benchmark <github@users.noreply.github.com>
2024-09-12 02:58:33 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 871bec22b92b8f343d3546f76c76826fa526aab1

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725897701473,
+  "lastUpdate": 1726109913486,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -8730,6 +8730,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.4234999999999998,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "871bec22b92b8f343d3546f76c76826fa526aab1",
+          "message": "Add test coverage to workflows\n\nThis change adds code coverage to our workflows and the necessary\nMakefile targets to support this.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-09-11T19:49:27-07:00",
+          "tree_id": "e46d04ff2bba5287f537efe72aabaa078d9b6d50",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/871bec22b92b8f343d3546f76c76826fa526aab1"
+        },
+        "date": 1726109909286,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 17.005499999999998,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.6135,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 3.0915,
             "unit": "Seconds",
             "extra": "P90"
           }
