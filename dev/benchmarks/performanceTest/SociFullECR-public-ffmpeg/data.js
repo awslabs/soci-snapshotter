b0d3a9e3a7876aa17f48d032cd1655d3ef9d4ca7
github-action-benchmark <github@users.noreply.github.com>
2024-10-07 19:52:38 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 97bb872478e308e22f93c01c7a77befa29a2dd6e

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728073843068,
+  "lastUpdate": 1728330758722,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -9063,6 +9063,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.65,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "97bb872478e308e22f93c01c7a77befa29a2dd6e",
+          "message": "Move TestSnapshotterStartup to startup_test.go\n\nThis change consolidates snapshotter startup tests into a single file\ninstead of putting it in pull_test.go\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2024-10-07T12:43:29-07:00",
+          "tree_id": "6c2508d1ccb170f4ec45e089ff7a79e1f12c0a5b",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/97bb872478e308e22f93c01c7a77befa29a2dd6e"
+        },
+        "date": 1728330753148,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1365,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.010499999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.1995,
             "unit": "Seconds",
             "extra": "P90"
           }
