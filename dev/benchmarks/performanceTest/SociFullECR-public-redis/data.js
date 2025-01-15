bcdbe226f31e34160790e15e85e0d999a2441922
github-action-benchmark <github@users.noreply.github.com>
2025-01-15 17:21:57 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 1b2442118aac93685d5a6f95ca6e49101a0e6f96

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736523465299,
+  "lastUpdate": 1736961716999,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -10028,6 +10028,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.3,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "1b2442118aac93685d5a6f95ca6e49101a0e6f96",
+          "message": "Bump dependencies using scripts/bump-deps.sh\n\nSigned-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-01-15T11:13:43-06:00",
+          "tree_id": "ed17c90070bfa100a2370bb18d00f7d2d8dc3f0f",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/1b2442118aac93685d5a6f95ca6e49101a0e6f96"
+        },
+        "date": 1736961715881,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 1.3519999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0235,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.3599999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
