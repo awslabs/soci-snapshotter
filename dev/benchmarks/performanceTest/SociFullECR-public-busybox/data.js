0fa5b60e51c5ebc4fc3bb2d95d5924b09cd68579
github-action-benchmark <github@users.noreply.github.com>
2025-01-15 23:14:58 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 4cfe4cc3e5c576737d20baaf95b6f45874b690d8

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736982412343,
+  "lastUpdate": 1736982898102,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -10071,6 +10071,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.169,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "4cfe4cc3e5c576737d20baaf95b6f45874b690d8",
+          "message": "Update Go 1.22.10 in CI\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-01-15T17:07:10-06:00",
+          "tree_id": "61da8f8612ea0ffe414518dc2e55c08c8eefe6d6",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/4cfe4cc3e5c576737d20baaf95b6f45874b690d8"
+        },
+        "date": 1736982895438,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.016,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.005,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 0.8005,
             "unit": "Seconds",
             "extra": "P90"
           }
