816f2b03627d4c10b86ae07b0607fdf7785c8672
github-action-benchmark <github@users.noreply.github.com>
2025-01-15 23:06:47 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 5f3847cb9ae490478b2ee05ef14af73d48d896c5

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736961721722,
+  "lastUpdate": 1736982407212,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -9988,6 +9988,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.42,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alvajus@amazon.com",
+            "name": "Justin Alvarez",
+            "username": "pendo324"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "5f3847cb9ae490478b2ee05ef14af73d48d896c5",
+          "message": "Allow setting version and revision\n\nSigned-off-by: Justin Alvarez <alvajus@amazon.com>",
+          "timestamp": "2025-01-15T14:58:16-08:00",
+          "tree_id": "7cce33c3aaf5e3a0876284cf113a632c7b56001c",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/5f3847cb9ae490478b2ee05ef14af73d48d896c5"
+        },
+        "date": 1736982404495,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.7774999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.322,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.3975,
             "unit": "Seconds",
             "extra": "P90"
           }
