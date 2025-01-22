4816d1a829cfe714686c680cb9ddc2f3b076907f
github-action-benchmark <github@users.noreply.github.com>
2025-01-22 18:16:07 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 4691e977191148fe01f6e5de35b1e01a5e87ded1

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737563231166,
+  "lastUpdate": 1737569767850,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -10196,6 +10196,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.311,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "4691e977191148fe01f6e5de35b1e01a5e87ded1",
+          "message": "Update Go 1.22.11 in CI\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-01-22T12:07:28-06:00",
+          "tree_id": "c89fc862fec0bf838a0398b9713b6a46d71c529f",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/4691e977191148fe01f6e5de35b1e01a5e87ded1"
+        },
+        "date": 1737569766792,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 1.9305,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.026,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.132,
             "unit": "Seconds",
             "extra": "P90"
           }
