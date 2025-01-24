f39bfdf02ef2d0996d4fdf9c257144512d15fba3
github-action-benchmark <github@users.noreply.github.com>
2025-01-24 17:30:58 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 3b846abeb4da3cf417227041f8bf0ffc9e06a3f7

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737739800187,
+  "lastUpdate": 1737739858643,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -10116,6 +10116,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.0084999999999997,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "3b846abeb4da3cf417227041f8bf0ffc9e06a3f7",
+          "message": "Remove allow_no_verification option\n\nallow_no_verification was an option that SOCI inherited from Stargz that\nis not used by SOCI. It wasn't well documented and was not connected to\nanything.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-01-24T11:22:04-06:00",
+          "tree_id": "a560fb987b66203b7a944bbda97fedc09dc99504",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/3b846abeb4da3cf417227041f8bf0ffc9e06a3f7"
+        },
+        "date": 1737739857521,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 4.41,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4775,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.4305000000000003,
             "unit": "Seconds",
             "extra": "P90"
           }
