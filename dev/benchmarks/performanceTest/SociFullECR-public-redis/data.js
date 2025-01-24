8e16bf1f5e1a48d123ec51a25d8ae394692ecb10
github-action-benchmark <github@users.noreply.github.com>
2025-01-24 17:30:01 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 986864c6532b8b391ac64c9cef029a6c1a78dd58

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737569767850,
+  "lastUpdate": 1737739801453,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -10238,6 +10238,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.132,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "986864c6532b8b391ac64c9cef029a6c1a78dd58",
+          "message": "Move io abstractions to ioutils\n\nSOCI has a couple of extensions to combine various `io` interfaces. This\nPR moves these under `ioutils` instead of defining them in the packages\nwhere they were first needed to encourage reuse.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-01-24T11:21:29-06:00",
+          "tree_id": "073280ae78771d5c5cea0d798348cdded3b05ca7",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/986864c6532b8b391ac64c9cef029a6c1a78dd58"
+        },
+        "date": 1737739793682,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 0.9455,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.026000000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.4235,
             "unit": "Seconds",
             "extra": "P90"
           }
