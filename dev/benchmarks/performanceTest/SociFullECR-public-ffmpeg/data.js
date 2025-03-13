916a80ff1bc4d126f6d0de6e843676dd74f2dd2c
github-action-benchmark <github@users.noreply.github.com>
2025-03-13 03:41:47 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f397d107aeb778b9bfdb7def5dfdd1c47609c59d

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741837238407,
+  "lastUpdate": 1741837307779,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -10365,6 +10365,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.96,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "f397d107aeb778b9bfdb7def5dfdd1c47609c59d",
+          "message": "Properly refresh expired tokens\n\nAs issue 8735 in containerd has been fixed, we can remove this\nworkaround to force credential refresh.\n\nFunnily enough, this logic was actually broken by another bugfix in\ncontainerd (issue 9274), so we're conveniently killing two birds with\none stone here :)\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-03-12T22:34:13-05:00",
+          "tree_id": "e2e5cd1b23a4ce2114f8d24c5a8e16aceccaff80",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f397d107aeb778b9bfdb7def5dfdd1c47609c59d"
+        },
+        "date": 1741837304541,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.137,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.012,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.399,
             "unit": "Seconds",
             "extra": "P90"
           }
