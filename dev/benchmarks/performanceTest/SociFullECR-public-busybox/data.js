24bca81a01cec450694f1a9f1cd1ee6c3b4eeed8
github-action-benchmark <github@users.noreply.github.com>
2025-03-13 20:50:08 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for ebc8e5718967fc6aa5a435ec9ce2fa5a1375d5cd

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741892413792,
+  "lastUpdate": 1741899008528,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -10491,6 +10491,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.0979999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "ebc8e5718967fc6aa5a435ec9ce2fa5a1375d5cd",
+          "message": "Bump golang.org/x/net from 0.33.0 to 0.37.0\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-03-13T15:41:47-05:00",
+          "tree_id": "af8cdceb25cc401535791f3b677c74c0603f4a74",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/ebc8e5718967fc6aa5a435ec9ce2fa5a1375d5cd"
+        },
+        "date": 1741899006434,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0135,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.007,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.074,
             "unit": "Seconds",
             "extra": "P90"
           }
