db49e42f92bcaaadf7ba8705bd0999872b2677ef
github-action-benchmark <github@users.noreply.github.com>
2025-04-17 04:36:53 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 4544842f4b54548facccdfa94f358f6fc6bf3518

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744760653356,
+  "lastUpdate": 1744864613874,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11078,6 +11078,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.5035,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "4544842f4b54548facccdfa94f358f6fc6bf3518",
+          "message": "Disable systemd tests on x86 codebuild\n\nsystemd tests fail on codebuild x86 runners due to some issue with\ncgroups v2. For now, disable the tests (they should still pass on ARM)\nso we're not blocking PRs while we investigate.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-04-16T21:28:12-07:00",
+          "tree_id": "574ba06596a078fed9eb3d5aaa2569c41ff5ba35",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/4544842f4b54548facccdfa94f358f6fc6bf3518"
+        },
+        "date": 1744864607488,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 20.5325,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.5915,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.6625,
             "unit": "Seconds",
             "extra": "P90"
           }
