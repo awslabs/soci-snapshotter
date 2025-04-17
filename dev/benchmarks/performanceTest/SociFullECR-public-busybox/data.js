367b3aadb8fd1f59626cca2b3ec2b8de155425da
github-action-benchmark <github@users.noreply.github.com>
2025-04-17 04:36:49 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 4544842f4b54548facccdfa94f358f6fc6bf3518

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744760669042,
+  "lastUpdate": 1744864609606,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11037,6 +11037,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 0.7095,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "4544842f4b54548facccdfa94f358f6fc6bf3518",
+          "message": "Disable systemd tests on x86 codebuild\n\nsystemd tests fail on codebuild x86 runners due to some issue with\ncgroups v2. For now, disable the tests (they should still pass on ARM)\nso we're not blocking PRs while we investigate.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-04-16T21:28:12-07:00",
+          "tree_id": "574ba06596a078fed9eb3d5aaa2569c41ff5ba35",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/4544842f4b54548facccdfa94f358f6fc6bf3518"
+        },
+        "date": 1744864607502,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0175,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 0.847,
             "unit": "Seconds",
             "extra": "P90"
           }
