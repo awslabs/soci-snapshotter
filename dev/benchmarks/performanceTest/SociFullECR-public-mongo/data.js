51acf2c289a7b00d83630e475b9e25c51b112405
github-action-benchmark <github@users.noreply.github.com>
2025-04-17 17:35:10 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 575576db28b6b50c313e17a77dcdc3dda8a0a2dc

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744864615431,
+  "lastUpdate": 1744911310056,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11038,6 +11038,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.2135,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "575576db28b6b50c313e17a77dcdc3dda8a0a2dc",
+          "message": "Update golang.org/x/net to v0.39.0\n\nThis is a manual dependency bump to patch CVE GO-2025-3595, since our\nautomated script doesn't auto-bump indirect dependencies.\n\nhttps://pkg.go.dev/vuln/GO-2025-3595\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-04-17T10:25:52-07:00",
+          "tree_id": "f33f8b2d723a67910167ab265347621f85cf23ff",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/575576db28b6b50c313e17a77dcdc3dda8a0a2dc"
+        },
+        "date": 1744911308819,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 8.927,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3095,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.7445,
             "unit": "Seconds",
             "extra": "P90"
           }
