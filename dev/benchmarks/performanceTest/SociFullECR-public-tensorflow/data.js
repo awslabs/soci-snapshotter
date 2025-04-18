cd082abe9d048aed763ef19dcdc3464696e6b821
github-action-benchmark <github@users.noreply.github.com>
2025-04-18 22:58:43 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f1dc793496e7fcda14974c9ef9fae36df78044cc

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744911317900,
+  "lastUpdate": 1745017123896,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11162,6 +11162,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.9405000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "f1dc793496e7fcda14974c9ef9fae36df78044cc",
+          "message": "Dockerfile: add igzip and rapidgzip to development container\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-04-18T17:51:06-05:00",
+          "tree_id": "6195d2cf98b1bb70618932924e92ef424f5a9899",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f1dc793496e7fcda14974c9ef9fae36df78044cc"
+        },
+        "date": 1745017120554,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 10.8675,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.5815,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.8940000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
