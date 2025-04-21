5926d1f53c8032408bff95bf6128330fd8f06888
github-action-benchmark <github@users.noreply.github.com>
2025-04-21 19:54:35 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for e155859d63cadcb6dbf02ad09cb70c6eea17185f

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745259307862,
+  "lastUpdate": 1745265275126,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11205,6 +11205,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.7525,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "e155859d63cadcb6dbf02ad09cb70c6eea17185f",
+          "message": "Use functional args for config in integ tests\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-04-21T12:46:57-07:00",
+          "tree_id": "632985df0688565cad6cc358ee34caaf07dc2aec",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/e155859d63cadcb6dbf02ad09cb70c6eea17185f"
+        },
+        "date": 1745265273616,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1235,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.0075,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.7555,
             "unit": "Seconds",
             "extra": "P90"
           }
