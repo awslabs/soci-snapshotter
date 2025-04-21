7d701db3984c4cee0b7d5fe826f12680f159c79b
github-action-benchmark <github@users.noreply.github.com>
2025-04-21 18:15:09 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 8674b00fbbc98df538c41f7820ed2d85a34c173d

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745017125763,
+  "lastUpdate": 1745259309249,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11122,6 +11122,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 0.9395,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "8674b00fbbc98df538c41f7820ed2d85a34c173d",
+          "message": "Dockerfile: build rapidgzip on ARM\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-04-21T11:07:08-07:00",
+          "tree_id": "3c93603f85fff35798dfe6f2d9ba08e56fa79c91",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/8674b00fbbc98df538c41f7820ed2d85a34c173d"
+        },
+        "date": 1745259301106,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.7485,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.2985,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.3225,
             "unit": "Seconds",
             "extra": "P90"
           }
