d114e4e55acb42f772cf943a13b35b74a2916888
github-action-benchmark <github@users.noreply.github.com>
2025-04-23 16:26:44 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 60475f67947df43dcb075e93b41fd9b4b5393906

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745377328672,
+  "lastUpdate": 1745425604315,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11289,6 +11289,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 0.792,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "60475f67947df43dcb075e93b41fd9b4b5393906",
+          "message": "Bump dependencies using scripts/bump-deps.sh\n\nSigned-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-04-23T09:18:04-07:00",
+          "tree_id": "cd3fdbd4a52073891e5ffcf230683dcada2fe49c",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/60475f67947df43dcb075e93b41fd9b4b5393906"
+        },
+        "date": 1745425602810,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.011,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.0055,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.312,
             "unit": "Seconds",
             "extra": "P90"
           }
