09bb053c4529a6a0c438f11982a9c67c7a84a851
github-action-benchmark <github@users.noreply.github.com>
2025-04-30 15:28:41 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d72b9cc31885f9f6192adceeea73bb8c6e3d07b2

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745604947907,
+  "lastUpdate": 1746026921243,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11373,6 +11373,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.2145000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "d72b9cc31885f9f6192adceeea73bb8c6e3d07b2",
+          "message": "ci: add containerd-2.1.0-rc.0 to integration test matrix\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-04-30T08:18:51-07:00",
+          "tree_id": "2a278881ca3a38b28bb8c43544c45f57e59bec29",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d72b9cc31885f9f6192adceeea73bb8c6e3d07b2"
+        },
+        "date": 1746026918223,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.011,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.7845,
             "unit": "Seconds",
             "extra": "P90"
           }
