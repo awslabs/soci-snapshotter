94d677a78fa80104e8b2914a7d0a88d320bf2ef5
github-action-benchmark <github@users.noreply.github.com>
2025-04-30 22:28:39 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 963edca6d4a103f15c04cfbb77ae3e17ee17f4cf

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746026915114,
+  "lastUpdate": 1746052119472,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11415,6 +11415,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 2.122,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "963edca6d4a103f15c04cfbb77ae3e17ee17f4cf",
+          "message": "Add SOCI conversion\n\nThis change adds a command to convert an image to a SOCI-enabled image which\ncontains strong references to both the image and SOCI index in a\nlogically single, deployable artifact.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-04-30T15:20:03-07:00",
+          "tree_id": "3e7f4b53bb17cf40556545332fb8370edda97667",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/963edca6d4a103f15c04cfbb77ae3e17ee17f4cf"
+        },
+        "date": 1746052117423,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1875,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.007,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 2.2279999999999998,
             "unit": "Seconds",
             "extra": "P90"
           }
