8b7d763760509c79b0f03d693b0bca256acdd05e
github-action-benchmark <github@users.noreply.github.com>
2025-04-30 15:28:37 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d72b9cc31885f9f6192adceeea73bb8c6e3d07b2

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745604944992,
+  "lastUpdate": 1746026917291,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11414,6 +11414,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.925,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "d72b9cc31885f9f6192adceeea73bb8c6e3d07b2",
+          "message": "ci: add containerd-2.1.0-rc.0 to integration test matrix\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-04-30T08:18:51-07:00",
+          "tree_id": "2a278881ca3a38b28bb8c43544c45f57e59bec29",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d72b9cc31885f9f6192adceeea73bb8c6e3d07b2"
+        },
+        "date": 1746026914123,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 34.1415,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.548,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.553,
             "unit": "Seconds",
             "extra": "P90"
           }
