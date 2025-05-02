5598cf1aa265c358a6e36ab59c5d3886903fa3fa
github-action-benchmark <github@users.noreply.github.com>
2025-05-02 21:21:10 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 82530d44ee2f477a9bbd2b821d6bfcb7711b5c16

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746132999944,
+  "lastUpdate": 1746220870330,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11458,6 +11458,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.1085,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "82530d44ee2f477a9bbd2b821d6bfcb7711b5c16",
+          "message": "Support finding SOCI indexes in image annotations\n\nThis change adds a new mechanism to discover SOCI indexes without the\nOCI referrers API via annotations on the image manifest. The new\nmechanism is called SOCI v2 while the original is called SOCI v1.\n\nBy default, SOCI v1 is disabled and SOCI v2 is enabled.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-05-02T14:12:37-07:00",
+          "tree_id": "c632f197ff6ca7ad554eb0d8642dc59ce2ad26bc",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/82530d44ee2f477a9bbd2b821d6bfcb7711b5c16"
+        },
+        "date": 1746220867155,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 5.3565000000000005,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3045,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.3165,
             "unit": "Seconds",
             "extra": "P90"
           }
