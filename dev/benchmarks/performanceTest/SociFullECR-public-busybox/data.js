7cb58fb0978de79c951dc24328b936be88eeb423
github-action-benchmark <github@users.noreply.github.com>
2025-05-05 17:28:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 2618de545e9cf5fbc1e4056540eb6743e14e0ace

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746220871317,
+  "lastUpdate": 1746466102208,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11541,6 +11541,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.1440000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "2618de545e9cf5fbc1e4056540eb6743e14e0ace",
+          "message": "Run benchmark workflows on change\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-05-05T10:20:28-07:00",
+          "tree_id": "7162068db80b9c20bbf33f10132c4a24fbd2a2fb",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/2618de545e9cf5fbc1e4056540eb6743e14e0ace"
+        },
+        "date": 1746466100250,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.048,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.0655000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
