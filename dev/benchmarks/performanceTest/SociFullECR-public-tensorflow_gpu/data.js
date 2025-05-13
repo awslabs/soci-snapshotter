ec534ca290265d442327302c88388e0e8b569d38
github-action-benchmark <github@users.noreply.github.com>
2025-05-13 22:46:42 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 9fae5811513832d244c90bbf7dcbdd7b52e10e64

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747147113094,
+  "lastUpdate": 1747176402356,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11712,6 +11712,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.803,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "9fae5811513832d244c90bbf7dcbdd7b52e10e64",
+          "message": "Bump dependencies using scripts/bump-deps.sh\n\nSigned-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-05-13T15:36:24-07:00",
+          "tree_id": "1d0c61c2b60667072c0c4ce7930b4e42dd004de4",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/9fae5811513832d244c90bbf7dcbdd7b52e10e64"
+        },
+        "date": 1747176400992,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 30.8345,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.705,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.6180000000000003,
             "unit": "Seconds",
             "extra": "P90"
           }
