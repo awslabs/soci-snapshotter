22d272c15beae4fc2160b3cacaf1d603619c406e
github-action-benchmark <github@users.noreply.github.com>
2025-05-13 14:38:31 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 9266a7cf122b1315e24dcf8f438137fc1d1976fe

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746723458962,
+  "lastUpdate": 1747147111165,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -11629,6 +11629,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.5605000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "9266a7cf122b1315e24dcf8f438137fc1d1976fe",
+          "message": "ci: update containerd@v2.1.0 integration testing\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-05-13T07:28:54-07:00",
+          "tree_id": "8677bfa4ee1c13bbda7e86ffc08fcff104764c79",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/9266a7cf122b1315e24dcf8f438137fc1d1976fe"
+        },
+        "date": 1747147109766,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 11.120999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 9.046,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 2.2239999999999998,
             "unit": "Seconds",
             "extra": "P90"
           }
