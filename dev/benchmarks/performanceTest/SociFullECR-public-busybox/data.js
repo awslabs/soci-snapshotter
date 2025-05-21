ebdee26cd6100024c7cf258878ef60fdf3197aa7
github-action-benchmark <github@users.noreply.github.com>
2025-05-21 21:30:19 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 16d71c28b17e959def9c226111f41df1b5e9bb3a

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747862290978,
+  "lastUpdate": 1747863019768,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12003,6 +12003,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.0795,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "16d71c28b17e959def9c226111f41df1b5e9bb3a",
+          "message": "Test invalid conversions\n\nIf a SOCI index or image manifest is removed from a converted image, the\nimage should be invalid. This change adds a test to confirm this.\n\nThis effectively tests that we are actually putting the SOCI index into\nthe converted Image Index.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-05-21T14:21:52-07:00",
+          "tree_id": "32133fe99e972c0c55692283735f359c3d170ec2",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/16d71c28b17e959def9c226111f41df1b5e9bb3a"
+        },
+        "date": 1747863018436,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.025500000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 0.7855,
             "unit": "Seconds",
             "extra": "P90"
           }
