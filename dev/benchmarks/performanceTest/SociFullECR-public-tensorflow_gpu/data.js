b03bc63f723c0b98b47b8c362a6bc8c7104a81b4
github-action-benchmark <github@users.noreply.github.com>
2025-05-28 22:42:58 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f53f34731338305393662163675d30dfe87fcc92

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748471780462,
+  "lastUpdate": 1748472178170,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12048,6 +12048,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.0965,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "f53f34731338305393662163675d30dfe87fcc92",
+          "message": "Fix TestOverlayView in userns\n\nTestOverlayView assumes that it is not run in a user namespace or that\n/tmp is a tmpfs. If it is run in a user namespace with a non-tmpfs /tmp,\nthen the mount contains an additional `userxattr` mount option. This\nchange checks if we expect that option and asserts on it as necessary.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-05-28T15:35:06-07:00",
+          "tree_id": "4cb81574241a33221a9c43a801c42789110a780a",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f53f34731338305393662163675d30dfe87fcc92"
+        },
+        "date": 1748472176681,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 10.711,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.662,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 1.963,
             "unit": "Seconds",
             "extra": "P90"
           }
