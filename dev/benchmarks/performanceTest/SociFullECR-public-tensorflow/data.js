c59650bdf79a2c11af89d6f7c6f0b4b1b3028561
github-action-benchmark <github@users.noreply.github.com>
2025-05-29 17:22:48 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for fa7d4ca92b444e71b4cd54c757e9165552838eed

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748472181410,
+  "lastUpdate": 1748539368013,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12254,6 +12254,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.7515,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "fa7d4ca92b444e71b4cd54c757e9165552838eed",
+          "message": "Use default platform in convert\n\nBefore this change, the SOCI cli defaulted to converting all platforms.\nThis is inconsistent with nerdctl's pull, push, and convert functions.\nIt also leads to weird issues like this not working:\n\n```\nnerdctl pull docker.io/library/busybox:latest\nsoci convert docker.io/library/busybox:latest other:latest-soci\n```\n\nthe solution is either to pull with `--all-platforms` or convert with\n`--platform`. The latter had an additional bug that convert touched all\nmanifests, even if you didn't convert them.\n\nThis change makes SOCI default to single platform conversions and fixes\nthe manifests bug. SOCI convert now behaves like nerdctl\npull/convert/push.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-05-29T10:13:03-07:00",
+          "tree_id": "5d9b8231688dd86e2e15b93e8556893a63e20b75",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/fa7d4ca92b444e71b4cd54c757e9165552838eed"
+        },
+        "date": 1748539366292,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 16.208,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.807,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.7925,
             "unit": "Seconds",
             "extra": "P90"
           }
