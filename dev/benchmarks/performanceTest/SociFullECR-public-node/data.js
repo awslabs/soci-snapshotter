7a59f5bfb855c1e6965eb3e7385ec809c8817616
github-action-benchmark <github@users.noreply.github.com>
2025-06-02 22:01:02 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 32dcd22b868132b63b974d6c346c84ae6eb50193

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748539367119,
+  "lastUpdate": 1748901662106,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12090,6 +12090,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.7145000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "macedonv@amazon.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "32dcd22b868132b63b974d6c346c84ae6eb50193",
+          "message": "Upgrade to github.com/pelletier/go-toml/v2\n\nSigned-off-by: Austin Vazquez <macedonv@amazon.com>",
+          "timestamp": "2025-06-02T14:50:16-07:00",
+          "tree_id": "2cdc09fcf7074d65bc4a0280ea198cec631d27cc",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/32dcd22b868132b63b974d6c346c84ae6eb50193"
+        },
+        "date": 1748901653350,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 5.1435,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4765,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.2815,
             "unit": "Seconds",
             "extra": "P90"
           }
