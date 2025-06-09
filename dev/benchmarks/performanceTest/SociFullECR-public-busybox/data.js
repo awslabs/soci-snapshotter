7b9d56a8955675ab643a476e840c38ef4967b731
github-action-benchmark <github@users.noreply.github.com>
2025-06-09 20:01:09 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d96cd2d58828d1d5ebe41a97d9ab4e421e9cac30

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749157261858,
+  "lastUpdate": 1749499269439,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12381,6 +12381,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 0.8065,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "d96cd2d58828d1d5ebe41a97d9ab4e421e9cac30",
+          "message": "Prevent pushing SOCI index manifest v2\n\nSOCI index manifest v2 does not have a subject field and instead relies\non a connection to an image via an image index. Before this change, you\ncould use `soci push` to push a SOCI index manifest v2 without the rest\nof the image which is unusable without the image index. This change\ndetects SOCI index manifest v2 and skips the push.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-06-09T12:51:20-07:00",
+          "tree_id": "89a210f1fc0d59a0459a3f2b1dc62caf32103954",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d96cd2d58828d1d5ebe41a97d9ab4e421e9cac30"
+        },
+        "date": 1749499259201,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0215,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.447,
             "unit": "Seconds",
             "extra": "P90"
           }
