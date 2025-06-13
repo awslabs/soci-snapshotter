e9dc4fdd9eb38220b036d224bb93765d7e82857f
github-action-benchmark <github@users.noreply.github.com>
2025-06-13 23:29:06 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for edb6dcfebbc00731896935d1701c5177a3e7069b

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749674054984,
+  "lastUpdate": 1749857346307,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12507,6 +12507,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.8645,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "edb6dcfebbc00731896935d1701c5177a3e7069b",
+          "message": "Add debug documentation for pull mode\n\nThis change adds information to debug how the SOCI snapshotter pulls an\nimage.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-06-13T16:20:58-07:00",
+          "tree_id": "00fc359dc03c99912c8e3e3d995fb7c661c8bc69",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/edb6dcfebbc00731896935d1701c5177a3e7069b"
+        },
+        "date": 1749857342420,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1305,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.007,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.7015,
             "unit": "Seconds",
             "extra": "P90"
           }
