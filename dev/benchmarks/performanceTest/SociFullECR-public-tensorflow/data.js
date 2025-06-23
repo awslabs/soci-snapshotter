4b595a9ebc6d82a338a05f1ace446d8d5f9545d1
github-action-benchmark <github@users.noreply.github.com>
2025-06-23 20:34:37 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 63e5cf953dd1e5c680d81af21e2702ad84db3314

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749857344115,
+  "lastUpdate": 1750710877969,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12590,6 +12590,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 2.153,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "63e5cf953dd1e5c680d81af21e2702ad84db3314",
+          "message": "Fix incorrect type check for soci ztoc info\n\nWe used to check the artifact's MediaType to determine if the artifact\nbeing passed to the command was a SOCI index or a zTOC, but the\nMediaType will always be the same regardless of the artifact type. This\nfixes the check to look for the artifact type instead of the MediaType.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-06-23T13:26:24-07:00",
+          "tree_id": "06584353512fe588ae9d3a06e00e0134b941ac2c",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/63e5cf953dd1e5c680d81af21e2702ad84db3314"
+        },
+        "date": 1750710868027,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 9.902000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.715,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.4235000000000002,
             "unit": "Seconds",
             "extra": "P90"
           }
