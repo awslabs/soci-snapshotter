3e0f93266c8aa0f040ea13615eb91ac743a5ccc2
github-action-benchmark <github@users.noreply.github.com>
2025-06-26 20:50:21 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 472f4e5b563910064e4d040e62f49cacfef9c134

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750967979935,
+  "lastUpdate": 1750971021517,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12511,6 +12511,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.127,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "472f4e5b563910064e4d040e62f49cacfef9c134",
+          "message": "Wait for containerd in systemd tests\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-06-26T13:41:29-07:00",
+          "tree_id": "b9a7433e4abc59ad139644acbc2afa270c74e199",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/472f4e5b563910064e4d040e62f49cacfef9c134"
+        },
+        "date": 1750971014308,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 14.5215,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 9.5195,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 1.1445,
             "unit": "Seconds",
             "extra": "P90"
           }
