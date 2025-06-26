011516393e5690f1cfe8eb87917e86a0418ea230
github-action-benchmark <github@users.noreply.github.com>
2025-06-26 19:59:38 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 506e318fcc768b1140b6cbd223baf0091bb2d0ee

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750963207866,
+  "lastUpdate": 1750967978148,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12674,6 +12674,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.51,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "506e318fcc768b1140b6cbd223baf0091bb2d0ee",
+          "message": "Bump golangci/golangci-lint-action from 6 to 7\n\nBumps [golangci/golangci-lint-action](https://github.com/golangci/golangci-lint-action) from 6 to 7.\n- [Release notes](https://github.com/golangci/golangci-lint-action/releases)\n- [Commits](https://github.com/golangci/golangci-lint-action/compare/v6...v7)\n\n---\nupdated-dependencies:\n- dependency-name: golangci/golangci-lint-action\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-06-26T12:50:44-07:00",
+          "tree_id": "d18d896c5ad491651868d9f6b9ffa45ef955f9aa",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/506e318fcc768b1140b6cbd223baf0091bb2d0ee"
+        },
+        "date": 1750967971998,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 0.7769999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0225,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.209,
             "unit": "Seconds",
             "extra": "P90"
           }
