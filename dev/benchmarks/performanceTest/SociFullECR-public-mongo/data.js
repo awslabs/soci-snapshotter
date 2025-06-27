ad6baac554336bd6bfa72b4262572753e8047b07
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 18:28:37 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 4a78fc699fb41ca6401ac9b457b11bde4014b257

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750971015742,
+  "lastUpdate": 1751048917614,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12676,6 +12676,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 0.851,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henwang@amazon.com",
+            "name": "Henry Wang",
+            "username": "henry118"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "4a78fc699fb41ca6401ac9b457b11bde4014b257",
+          "message": "refactor parallel pull mode configs\n\nSigned-off-by: Henry Wang <henwang@amazon.com>",
+          "timestamp": "2025-06-27T11:20:02-07:00",
+          "tree_id": "2593c5bc40b4cfabc52bbd6d4ae070eaa51fd393",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/4a78fc699fb41ca6401ac9b457b11bde4014b257"
+        },
+        "date": 1751048912954,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.4805000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.303,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.1935,
             "unit": "Seconds",
             "extra": "P90"
           }
