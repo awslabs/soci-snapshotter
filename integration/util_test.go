61ffc0cfb08f32a49261aa0b44f537e8ae1d3737
David Son <davbson@amazon.com>
2025-07-18 19:47:42 +0000

    Move some ParallelPull integration to helper funcs
    Signed-off-by: David Son <davbson@amazon.com>

--- integration/util_test.go
+++ integration/util_test.go
@@ -377,6 +377,18 @@ func withUnboundedPullUnpack() snapshotterConfigOpt {
 	}
 }
 
+func withConcurrentDownloadChunkSize(chunkSize int64) snapshotterConfigOpt {
+	return func(c *config.Config) {
+		c.PullModes.Parallel.ConcurrentDownloadChunkSize = chunkSize
+	}
+}
+
+func withConcurrentUnpacks(n int64) snapshotterConfigOpt {
+	return func(c *config.Config) {
+		c.PullModes.Parallel.MaxConcurrentUnpacks = n
+	}
+}
+
 func withDiscardUnpackedLayers() snapshotterConfigOpt {
 	return func(c *config.Config) {
 		c.PullModes.Parallel.DiscardUnpackedLayers = true
