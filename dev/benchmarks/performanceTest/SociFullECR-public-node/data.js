084054cea8d72f0a4d2f5459dd14b7f3b715b5fb
github-action-benchmark <github@users.noreply.github.com>
2025-07-21 18:36:16 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 73dd46eed7a77f5f5b454eaeb1c227fb5efab320

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752527458757,
+  "lastUpdate": 1753122976030,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -12846,6 +12846,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.0385,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "73dd46eed7a77f5f5b454eaeb1c227fb5efab320",
+          "message": "Hard-fail on config parsing errors\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-07-21T11:27:20-07:00",
+          "tree_id": "8001452945121a6ee88368ec2794c0b4079fd070",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/73dd46eed7a77f5f5b454eaeb1c227fb5efab320"
+        },
+        "date": 1753122974023,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 1.749,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.5105,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.1310000000000002,
             "unit": "Seconds",
             "extra": "P90"
           }
