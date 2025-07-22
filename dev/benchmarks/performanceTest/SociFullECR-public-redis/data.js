bae6bffdba1c3c30b44a28086ca23e24c469068c
github-action-benchmark <github@users.noreply.github.com>
2025-07-22 22:04:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 790a445854406da5b174b267104be43a01bd37ec

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753122980031,
+  "lastUpdate": 1753221862587,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13094,6 +13094,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.593,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "790a445854406da5b174b267104be43a01bd37ec",
+          "message": "Only instantiate parallel pull structs if enabled\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-07-22T14:54:15-07:00",
+          "tree_id": "554d7fb68d9252d04c2f4f941376a403be1d0df7",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/790a445854406da5b174b267104be43a01bd37ec"
+        },
+        "date": 1753221853630,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 1.6355,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.025,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.1775,
             "unit": "Seconds",
             "extra": "P90"
           }
