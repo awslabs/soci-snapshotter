d831a27d3670dfdcd6b6964a0203bf4c16c10347
github-action-benchmark <github@users.noreply.github.com>
2025-07-30 17:29:36 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f698d94a448f4dd4152bc8695ce846e68bab30ad

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753828584856,
+  "lastUpdate": 1753896576442,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13015,6 +13015,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.627,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "f698d94a448f4dd4152bc8695ce846e68bab30ad",
+          "message": "Move the snapshotter to urfave\n\nBefore this change, the SOCI CLI used urfave CLI, but the snapshotter\nused go's flag package. This moved the snapshotter to urfave as well.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-07-30T10:21:35-07:00",
+          "tree_id": "9dfb8fe5cd1914660894cc025edca15a69533493",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f698d94a448f4dd4152bc8695ce846e68bab30ad"
+        },
+        "date": 1753896571429,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 12.1705,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 10.2165,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 0.9219999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
