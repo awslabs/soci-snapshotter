c05092bf22b6a6be2c7a68f5196bc1ec9941beea
github-action-benchmark <github@users.noreply.github.com>
2025-08-05 21:59:02 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for a998022a174bc2ddc6c258c6d880f663c9664b2a

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754431136623,
+  "lastUpdate": 1754431142007,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13348,6 +13348,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.498,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "a998022a174bc2ddc6c258c6d880f663c9664b2a",
+          "message": "Update dependabot.yml and remove custom workflow\n\nSince June 25 2024, dependabot has added multi-directory support for\ndependencies. As the existing bump-deps.yml was meant as a workaround\nfor this, we can safely remove it and rely solely on dependabot instead.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-08-05T14:49:51-07:00",
+          "tree_id": "ad979db0e6230edc269950a46486bbfe9aa8fdce",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/a998022a174bc2ddc6c258c6d880f663c9664b2a"
+        },
+        "date": 1754431139641,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 2.3120000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.5165,
             "unit": "Seconds",
             "extra": "P90"
           }
