eee11ce2d0b111480cb751c54233cd3924faec08
github-action-benchmark <github@users.noreply.github.com>
2025-08-05 21:59:15 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 5570f817ea39b5c4d73f8fce3efaa80cbb1f2074

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754431142007,
+  "lastUpdate": 1754431155620,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13390,6 +13390,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.5165,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "5570f817ea39b5c4d73f8fce3efaa80cbb1f2074",
+          "message": "Enable copyloopvar rule in Go linter\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-08-05T14:50:46-07:00",
+          "tree_id": "cb554bad5677b5e696d3b197f813b2e6d03f55c2",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/5570f817ea39b5c4d73f8fce3efaa80cbb1f2074"
+        },
+        "date": 1754431153783,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.775,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3035,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.4745,
             "unit": "Seconds",
             "extra": "P90"
           }
