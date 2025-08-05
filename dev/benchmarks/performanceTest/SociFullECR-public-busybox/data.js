db483ca4e3323c45020a0a9aaa04d6674df8ae40
github-action-benchmark <github@users.noreply.github.com>
2025-08-05 21:59:07 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 3008f273c357b986dbd571fdd709d7d4b6921881

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754431140693,
+  "lastUpdate": 1754431147861,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13347,6 +13347,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.218,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "3008f273c357b986dbd571fdd709d7d4b6921881",
+          "message": "Use -q flag when pulling with nerdctl in tests\n\nThis should hopefully greatly reduce the number of log lines we get.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-08-05T14:50:23-07:00",
+          "tree_id": "60f1ba9189bb5598a12ec25147fc8095872585d8",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/3008f273c357b986dbd571fdd709d7d4b6921881"
+        },
+        "date": 1754431134686,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.020499999999999997,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006500000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.35,
             "unit": "Seconds",
             "extra": "P90"
           }
