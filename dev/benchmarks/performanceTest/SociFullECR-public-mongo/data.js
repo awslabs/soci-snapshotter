baae51f969d9a0ef486f6094efa323e898b4c434
github-action-benchmark <github@users.noreply.github.com>
2025-08-05 18:24:03 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 6d62f0b50968dc378576edd6b82eaa876b2f4e24

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753914648075,
+  "lastUpdate": 1754418243589,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13264,6 +13264,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.3535,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "6d62f0b50968dc378576edd6b82eaa876b2f4e24",
+          "message": "Bump dependencies using scripts/bump-deps.sh\n\nSigned-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-08-05T11:13:32-07:00",
+          "tree_id": "c5f852b9ffb81b8eeaa5e4ce432332ec8f8ff790",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/6d62f0b50968dc378576edd6b82eaa876b2f4e24"
+        },
+        "date": 1754418238285,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 3.257,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3275,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.512,
             "unit": "Seconds",
             "extra": "P90"
           }
