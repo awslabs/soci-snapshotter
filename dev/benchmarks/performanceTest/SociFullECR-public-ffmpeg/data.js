bf3ae1c5a086a08d2de271c766e2423a14a373fb
github-action-benchmark <github@users.noreply.github.com>
2025-08-06 22:05:25 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 45a548c076b82d883b887e87a014af0f8fe2871a

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754434263498,
+  "lastUpdate": 1754517925634,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13515,6 +13515,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 2.0305,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "45a548c076b82d883b887e87a014af0f8fe2871a",
+          "message": "Reduce integration test log noise\n\nThis change reduces the integration test log volume by only printing\nlogs when a test fails. Even when the tests are run with `go test -v`,\nthe logs from docker compose, soci, and all the shell commands will be\nsurpressed unless the test fails.\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-08-06T14:56:36-07:00",
+          "tree_id": "691aaaa3d1bc0cc95f666d57848cb4bedc1bc403",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/45a548c076b82d883b887e87a014af0f8fe2871a"
+        },
+        "date": 1754517919942,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.4375,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.007,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.346,
             "unit": "Seconds",
             "extra": "P90"
           }
