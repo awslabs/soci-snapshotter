6f9e2a557182e292cbbc078018d036930db1120c
github-action-benchmark <github@users.noreply.github.com>
2025-08-13 17:01:10 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 6036fdc455e1f778d79b99eae1064d677f923aca

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754608168284,
+  "lastUpdate": 1755104470585,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13640,6 +13640,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.4740000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "6036fdc455e1f778d79b99eae1064d677f923aca",
+          "message": "Revert \"Update dependabot.yml and remove custom workflow\"\n\nThis reverts commit a998022a174bc2ddc6c258c6d880f663c9664b2a.\n\nUnfortunately dependabot does not recursively run go mod tidy for nested\ngomodules (e.g.: https://github.com/awslabs/soci-snapshotter/pull/1673)\n\nThere is currently an issue open for this, once it is resolved we can\nprobably un-revert this.\n\nIssue: https://github.com/dependabot/dependabot-core/issues/11046\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-08-13T12:53:08-04:00",
+          "tree_id": "39f37dd784bce63bccf61c42e303550420d02bc3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/6036fdc455e1f778d79b99eae1064d677f923aca"
+        },
+        "date": 1755104468788,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 10.0135,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.7385,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.4055,
             "unit": "Seconds",
             "extra": "P90"
           }
