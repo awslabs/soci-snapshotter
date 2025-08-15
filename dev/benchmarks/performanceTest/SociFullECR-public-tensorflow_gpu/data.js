a35f1f9c95b8aedcd0ad7172966ec99ca7223ebd
github-action-benchmark <github@users.noreply.github.com>
2025-08-15 21:02:17 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 6f4ed94a36d94e583749fc0e1e68a1d69ccaed5d

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755192495528,
+  "lastUpdate": 1755291737877,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13560,6 +13560,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.277,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil-Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "6f4ed94a36d94e583749fc0e1e68a1d69ccaed5d",
+          "message": "Changes:\n\n- Remove filenames from `metadata/reader` error logs\n- Remove filenames from `metadata/db` error logs\n\nSigned-off-by: Swapnanil-Gupta <swpnlg@amazon.com>",
+          "timestamp": "2025-08-15T13:54:02-07:00",
+          "tree_id": "0a9ebcb8ede21a877c3475735854f571a78492b5",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/6f4ed94a36d94e583749fc0e1e68a1d69ccaed5d"
+        },
+        "date": 1755291729878,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 11.922,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.6925,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.1195,
             "unit": "Seconds",
             "extra": "P90"
           }
