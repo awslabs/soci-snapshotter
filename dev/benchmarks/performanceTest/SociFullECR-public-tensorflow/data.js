79314eeee23d64f4bc8bc03b5c5f3239d0d26c37
github-action-benchmark <github@users.noreply.github.com>
2025-08-18 20:57:55 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7d775f472842a9ae96744d8529bbcd6a266971fe

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755541512482,
+  "lastUpdate": 1755550674995,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13850,6 +13850,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.7155,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henwang@amazon.com",
+            "name": "Henry Wang",
+            "username": "henry118"
+          },
+          "committer": {
+            "email": "wh_henry@hotmail.com",
+            "name": "Henry Wang",
+            "username": "henry118"
+          },
+          "distinct": true,
+          "id": "7d775f472842a9ae96744d8529bbcd6a266971fe",
+          "message": "Async digest verification\n\nSigned-off-by: Henry Wang <henwang@amazon.com>",
+          "timestamp": "2025-08-18T13:49:13-07:00",
+          "tree_id": "102194b97184a9f74ad70280cb393088606f6d08",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7d775f472842a9ae96744d8529bbcd6a266971fe"
+        },
+        "date": 1755550668940,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 9.642,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.646,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.0735,
             "unit": "Seconds",
             "extra": "P90"
           }
