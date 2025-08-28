9c11194c581bfd30b346b9ad9f7d0126a25f7405
github-action-benchmark <github@users.noreply.github.com>
2025-08-28 20:42:37 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for a6df487780834e6bb8af2b4c7cfa4a0fd0a6c133

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756331722150,
+  "lastUpdate": 1756413757079,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13896,6 +13896,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 1.6755,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "austin.vazquez.dev@gmail.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "a6df487780834e6bb8af2b4c7cfa4a0fd0a6c133",
+          "message": "Migrate soci command to urfave/cli/v3\n\nSigned-off-by: Austin Vazquez <austin.vazquez.dev@gmail.com>\nCo-authored-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-08-28T13:32:59-07:00",
+          "tree_id": "55106c29d7cddd5f26310274ebeab83d5da2faf3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/a6df487780834e6bb8af2b4c7cfa4a0fd0a6c133"
+        },
+        "date": 1756413753027,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 26.1135,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.6425,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.9330000000000003,
             "unit": "Seconds",
             "extra": "P90"
           }
