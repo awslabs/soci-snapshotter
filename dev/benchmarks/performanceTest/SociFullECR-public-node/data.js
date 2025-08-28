d105157eea316ba7357dbbff72a28f68419af92a
github-action-benchmark <github@users.noreply.github.com>
2025-08-28 20:42:33 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for a6df487780834e6bb8af2b4c7cfa4a0fd0a6c133

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756331723424,
+  "lastUpdate": 1756413753897,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -13854,6 +13854,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.474,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "austin.vazquez.dev@gmail.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "a6df487780834e6bb8af2b4c7cfa4a0fd0a6c133",
+          "message": "Migrate soci command to urfave/cli/v3\n\nSigned-off-by: Austin Vazquez <austin.vazquez.dev@gmail.com>\nCo-authored-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2025-08-28T13:32:59-07:00",
+          "tree_id": "55106c29d7cddd5f26310274ebeab83d5da2faf3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/a6df487780834e6bb8af2b4c7cfa4a0fd0a6c133"
+        },
+        "date": 1756413752024,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 2.885,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.47050000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.9369999999999998,
             "unit": "Seconds",
             "extra": "P90"
           }
