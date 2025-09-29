733a4f43e7ba81fa6a84514eba349691a76baedf
github-action-benchmark <github@users.noreply.github.com>
2025-09-29 18:16:12 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 586e301cdbc9fa58e7e05e7e12f30f13b041d7d9

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757962030604,
+  "lastUpdate": 1759169772923,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -14271,6 +14271,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.6555,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "github@adolfo.ochagavia.nl",
+            "name": "Adolfo Ochagavía",
+            "username": "aochagavia"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "586e301cdbc9fa58e7e05e7e12f30f13b041d7d9",
+          "message": "Actually check the download's sha256 checksum in parallel-mode-install.sh\n\nThe script was mistakingly _calculating_ the sha256 hash of the checksum, instead of _using_ the checksum to check against the downloaded file.\n\nSigned-off-by: Adolfo Ochagavía <github@adolfo.ochagavia.nl>",
+          "timestamp": "2025-09-29T14:06:57-04:00",
+          "tree_id": "ed4aa1beb29849b7b25244bd09f73541c1ccefe1",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/586e301cdbc9fa58e7e05e7e12f30f13b041d7d9"
+        },
+        "date": 1759169768768,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1615,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.007,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.9980000000000002,
             "unit": "Seconds",
             "extra": "P90"
           }
