32725fa81f8f56cb6fe129c2e74c2e50d0fcbd95
github-action-benchmark <github@users.noreply.github.com>
2025-10-15 22:28:47 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f044f74a8d0b33b259628c9c3ae4fab93979ec05

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760551285618,
+  "lastUpdate": 1760567327581,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -14232,6 +14232,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.4855,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "f044f74a8d0b33b259628c9c3ae4fab93979ec05",
+          "message": "ci: Properly check if Docker Compose is installed\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-10-15T18:19:06-04:00",
+          "tree_id": "6b16d0daa0405e9ceefd82fda38b82ad832b8680",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f044f74a8d0b33b259628c9c3ae4fab93979ec05"
+        },
+        "date": 1760567320152,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 4.521,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4775,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.5715,
             "unit": "Seconds",
             "extra": "P90"
           }
