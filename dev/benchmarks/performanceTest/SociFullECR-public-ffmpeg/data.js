48b9e9346417326a2e53845286a08eb00a6f5f25
github-action-benchmark <github@users.noreply.github.com>
2025-10-30 17:05:59 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 3fe7f855cea9fc0b1eb448e1d707a809b0058561

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761762986129,
+  "lastUpdate": 1761843959643,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -14733,6 +14733,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.5590000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil-Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "committer": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "distinct": true,
+          "id": "3fe7f855cea9fc0b1eb448e1d707a809b0058561",
+          "message": "feat: skip existing ztoc generation\n\nSigned-off-by: Swapnanil-Gupta <swpnlg@amazon.com>",
+          "timestamp": "2025-10-30T09:39:59-07:00",
+          "tree_id": "9d97f8bfc134d3e2d088b8222e88251a1beabf9f",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/3fe7f855cea9fc0b1eb448e1d707a809b0058561"
+        },
+        "date": 1761843953070,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.361,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.008,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 2.319,
             "unit": "Seconds",
             "extra": "P90"
           }
