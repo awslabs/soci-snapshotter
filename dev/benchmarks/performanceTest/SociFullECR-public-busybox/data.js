0d7db76e3d21c02d84b4fdfea81f0f65ee6b0729
github-action-benchmark <github@users.noreply.github.com>
2025-11-11 00:28:47 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 88fe5688e9adba8232e2f72053919bd3a5e53a4b

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762197718397,
+  "lastUpdate": 1762820927713,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -14775,6 +14775,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.4765,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "88fe5688e9adba8232e2f72053919bd3a5e53a4b",
+          "message": "Remove Go patch version requirement\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-11-10T19:20:37-05:00",
+          "tree_id": "ab4f34126cedb40bbd3cbb24f627e8d0e5ca2154",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/88fe5688e9adba8232e2f72053919bd3a5e53a4b"
+        },
+        "date": 1762820924839,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0205,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.0055,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.0405,
             "unit": "Seconds",
             "extra": "P90"
           }
