5a2793017a831fd39788377bdf4c3676d49ec366
github-action-benchmark <github@users.noreply.github.com>
2025-11-12 19:11:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for e97b5e9fa021b2015b55e16765c798abd7efee79

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762974651919,
+  "lastUpdate": 1762974682932,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -14736,6 +14736,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 4.023,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abushwang@tencent.com",
+            "name": "abushwang"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "e97b5e9fa021b2015b55e16765c798abd7efee79",
+          "message": "refactor newNode to simplify function header\n\nSigned-off-by: abushwang <abushwang@tencent.com>",
+          "timestamp": "2025-11-12T14:01:48-05:00",
+          "tree_id": "b6e58cc6ae9bd1c2544641f97d01596d0e005ae6",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/e97b5e9fa021b2015b55e16765c798abd7efee79"
+        },
+        "date": 1762974674452,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 4.97,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.486,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.495,
             "unit": "Seconds",
             "extra": "P90"
           }
