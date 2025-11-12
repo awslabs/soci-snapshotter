8506c696055c1049d915b682f63354662fedd30b
github-action-benchmark <github@users.noreply.github.com>
2025-11-12 19:11:19 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for e97b5e9fa021b2015b55e16765c798abd7efee79

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762974655018,
+  "lastUpdate": 1762974679781,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -14778,6 +14778,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 1.7085000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abushwang@tencent.com",
+            "name": "abushwang"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "e97b5e9fa021b2015b55e16765c798abd7efee79",
+          "message": "refactor newNode to simplify function header\n\nSigned-off-by: abushwang <abushwang@tencent.com>",
+          "timestamp": "2025-11-12T14:01:48-05:00",
+          "tree_id": "b6e58cc6ae9bd1c2544641f97d01596d0e005ae6",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/e97b5e9fa021b2015b55e16765c798abd7efee79"
+        },
+        "date": 1762974674235,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 20.4985,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.687,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.0035,
             "unit": "Seconds",
             "extra": "P90"
           }
