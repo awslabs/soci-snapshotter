2a4d19674f1d00a4a378e5198d9b900e245cad80
github-action-benchmark <github@users.noreply.github.com>
2025-11-17 22:35:31 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for a6982c310a92fa118aa3bf72b48b496b195f257d

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762974678679,
+  "lastUpdate": 1763418931490,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -14778,6 +14778,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.4195,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "a6982c310a92fa118aa3bf72b48b496b195f257d",
+          "message": "Lower Error to Warn in remote mount id-mapping\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2025-11-17T17:25:22-05:00",
+          "tree_id": "6ead4b035fa097befe85113be9de81916a866377",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/a6982c310a92fa118aa3bf72b48b496b195f257d"
+        },
+        "date": 1763418925930,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 15.502,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 10.1405,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 1.505,
             "unit": "Seconds",
             "extra": "P90"
           }
