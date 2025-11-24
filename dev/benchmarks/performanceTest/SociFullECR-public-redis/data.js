33dce4b371cc02ae2529c065aa88aa05a4440be6
github-action-benchmark <github@users.noreply.github.com>
2025-11-24 16:19:43 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7a6464abb91054de30797d02d70475d0f5f3ce2e

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763680697917,
+  "lastUpdate": 1764001183917,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -15025,6 +15025,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.349,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henwang@amazon.com",
+            "name": "Henry Wang",
+            "username": "henry118"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "7a6464abb91054de30797d02d70475d0f5f3ce2e",
+          "message": "Make sure draining the resp body for range-gets\n\nSigned-off-by: Henry Wang <henwang@amazon.com>",
+          "timestamp": "2025-11-24T11:10:15-05:00",
+          "tree_id": "94f56140fc11d809abac787e0c5333cb992fdcde",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7a6464abb91054de30797d02d70475d0f5f3ce2e"
+        },
+        "date": 1764001178175,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 1.233,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.025500000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.4855,
             "unit": "Seconds",
             "extra": "P90"
           }
