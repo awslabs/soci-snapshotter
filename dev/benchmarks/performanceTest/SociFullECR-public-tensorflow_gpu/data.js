d45e81755500e6586a0e026bf8e1165d6464c032
github-action-benchmark <github@users.noreply.github.com>
2025-11-24 16:19:40 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7a6464abb91054de30797d02d70475d0f5f3ce2e

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763680702125,
+  "lastUpdate": 1764001180486,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -14903,6 +14903,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.5475,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henwang@amazon.com",
+            "name": "Henry Wang",
+            "username": "henry118"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "7a6464abb91054de30797d02d70475d0f5f3ce2e",
+          "message": "Make sure draining the resp body for range-gets\n\nSigned-off-by: Henry Wang <henwang@amazon.com>",
+          "timestamp": "2025-11-24T11:10:15-05:00",
+          "tree_id": "94f56140fc11d809abac787e0c5333cb992fdcde",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7a6464abb91054de30797d02d70475d0f5f3ce2e"
+        },
+        "date": 1764001177070,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 19.8405,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.806,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.266,
             "unit": "Seconds",
             "extra": "P90"
           }
