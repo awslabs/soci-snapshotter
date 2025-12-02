d1f690b755074e9a68d2d6635982c94f273ed971
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 20:00:57 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for c1111b599e2576ca1c0ba85c49abf7ed026579d9

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764106746866,
+  "lastUpdate": 1764705657947,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -15363,6 +15363,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.5425,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil-Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "committer": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "distinct": true,
+          "id": "c1111b599e2576ca1c0ba85c49abf7ed026579d9",
+          "message": "fix: skip processing ztocs/indexes that are found in the artifacts store but not in the content store\n\nSigned-off-by: Swapnanil-Gupta <swpnlg@amazon.com>",
+          "timestamp": "2025-12-02T11:51:24-08:00",
+          "tree_id": "c5035b4f1349cc4f6e3f04f86924961a407c3ae5",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/c1111b599e2576ca1c0ba85c49abf7ed026579d9"
+        },
+        "date": 1764705650092,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.4885,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.294,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.1215,
             "unit": "Seconds",
             "extra": "P90"
           }
