b81572a8bb5a0f6f3344a0d0b35ffda74db3a1d4
github-action-benchmark <github@users.noreply.github.com>
2025-12-08 23:28:04 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for fa65e2c694b5110d22cf3ec46f01006b609af846

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764705653513,
+  "lastUpdate": 1765236483994,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -15323,6 +15323,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.3529999999999998,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun Raja Yogidas",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": true,
+          "id": "fa65e2c694b5110d22cf3ec46f01006b609af846",
+          "message": "add garbage collection labels to image mainfest\n\nSigned-off-by: Arjun Raja Yogidas <arjunry@amazon.com>",
+          "timestamp": "2025-12-08T15:19:51-08:00",
+          "tree_id": "cabf228f4b6dfcb9ab0dac7cc02dfa334d927435",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/fa65e2c694b5110d22cf3ec46f01006b609af846"
+        },
+        "date": 1765236478925,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 13.339500000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.659,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.261,
             "unit": "Seconds",
             "extra": "P90"
           }
