eb451a5353432273d2d510dba7bb927b6ebe81b3
github-action-benchmark <github@users.noreply.github.com>
2025-12-09 21:22:26 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 98f63cb073e86dfea11f2c0b3f6057816cd0f9b2

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765240137839,
+  "lastUpdate": 1765315346959,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -15407,6 +15407,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.7635,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil-Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "committer": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "distinct": true,
+          "id": "98f63cb073e86dfea11f2c0b3f6057816cd0f9b2",
+          "message": "Upgrade go to 1.24.11 in CI\n\nSigned-off-by: Swapnanil-Gupta <swpnlg@amazon.com>",
+          "timestamp": "2025-12-09T13:13:04-08:00",
+          "tree_id": "034e6989459a389292bcbafd48c2b7c797a0ce40",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/98f63cb073e86dfea11f2c0b3f6057816cd0f9b2"
+        },
+        "date": 1765315341023,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 19.003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.633,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.498,
             "unit": "Seconds",
             "extra": "P90"
           }
