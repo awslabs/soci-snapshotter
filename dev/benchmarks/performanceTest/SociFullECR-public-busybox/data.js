11eda66c2d8d7cdee3b27eb7289da85f29e5bba5
github-action-benchmark <github@users.noreply.github.com>
2025-12-22 23:04:54 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for b558f5960fcdcd0253638b76051a270ae4e469e1

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766046749015,
+  "lastUpdate": 1766444694278,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -15739,6 +15739,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.3635000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abushwang@tencent.com",
+            "name": "abushwang"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "b558f5960fcdcd0253638b76051a270ae4e469e1",
+          "message": "fix fscache not cleanup\n\nSigned-off-by: Shubhranshu Mahapatra <shubhum@amazon.com>\nSigned-off-by: abushwang <abushwang@tencent.com>",
+          "timestamp": "2025-12-22T17:55:41-05:00",
+          "tree_id": "9b493baa65938fef709b511f5a48aa48bc50d4a3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/b558f5960fcdcd0253638b76051a270ae4e469e1"
+        },
+        "date": 1766444690108,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.013999999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.005,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.2195,
             "unit": "Seconds",
             "extra": "P90"
           }
