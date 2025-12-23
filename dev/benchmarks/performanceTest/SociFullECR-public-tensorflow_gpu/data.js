1884dfe56f68bdadfc8fbdaf6885d328de963e38
github-action-benchmark <github@users.noreply.github.com>
2025-12-23 22:44:03 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 5cf1cdebb16ceea78f3a3b7ec8cd7a44e9032a92

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766444691991,
+  "lastUpdate": 1766529843062,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -15699,6 +15699,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.3025,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hudson@cyzhu.com",
+            "name": "ChengyuZhu6",
+            "username": "ChengyuZhu6"
+          },
+          "committer": {
+            "email": "kern.walster@gmail.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "distinct": true,
+          "id": "5cf1cdebb16ceea78f3a3b7ec8cd7a44e9032a92",
+          "message": "integration: add prefetch integration test\n\nSigned-off-by: ChengyuZhu6 <hudson@cyzhu.com>",
+          "timestamp": "2025-12-23T14:35:17-08:00",
+          "tree_id": "c53d1dfe7889cf31b85c6cbb06a16174673f56b2",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/5cf1cdebb16ceea78f3a3b7ec8cd7a44e9032a92"
+        },
+        "date": 1766529833606,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 12.9815,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.7595,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 3.305,
             "unit": "Seconds",
             "extra": "P90"
           }
