a2f4791c97992abd4b6ad185f33c1af943296c78
github-action-benchmark <github@users.noreply.github.com>
2025-12-25 02:49:18 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 57d438ae16634ef96a501280ca8bc47c962ca236

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766630882032,
+  "lastUpdate": 1766630958442,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -15989,6 +15989,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.3014999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abushwang@tencent.com",
+            "name": "abushwang"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "57d438ae16634ef96a501280ca8bc47c962ca236",
+          "message": "fix build error with strict build flag\n\nSigned-off-by: abushwang <abushwang@tencent.com>",
+          "timestamp": "2025-12-24T21:41:43-05:00",
+          "tree_id": "5ff9b8c03c7a7afcec1ed459d2bd85a64d0e70fe",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/57d438ae16634ef96a501280ca8bc47c962ca236"
+        },
+        "date": 1766630955273,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 0.4625,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0235,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.2734999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
