1082e27f383bf3483a3359a1c5c2b40e824184c8
github-action-benchmark <github@users.noreply.github.com>
2026-01-12 22:46:18 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for cf608324a19bae564275b6d3bc9a31917eb9e9e1

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767732688305,
+  "lastUpdate": 1768257978164,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -15909,6 +15909,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.7445,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michal@baumgartner.io",
+            "name": "Michal Baumgartner",
+            "username": "m1so"
+          },
+          "committer": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "distinct": true,
+          "id": "cf608324a19bae564275b6d3bc9a31917eb9e9e1",
+          "message": "Allow setting CLI flags via environment variables\n\nSigned-off-by: Michal Baumgartner <michal@baumgartner.io>",
+          "timestamp": "2026-01-12T14:36:39-08:00",
+          "tree_id": "1c608214cfecdb2f8a9281412995c0c75bb6ab75",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/cf608324a19bae564275b6d3bc9a31917eb9e9e1"
+        },
+        "date": 1768257971278,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 12.6045,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 9.583,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 1.2495,
             "unit": "Seconds",
             "extra": "P90"
           }
