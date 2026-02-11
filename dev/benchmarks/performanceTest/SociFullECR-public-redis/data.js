3c835624d882dfc33b19b6fed09152764118f0b3
github-action-benchmark <github@users.noreply.github.com>
2026-02-11 23:35:59 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d9d125ca3a423b53405b4aeeb362d6ec1f133ba9

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770843598240,
+  "lastUpdate": 1770852959747,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16240,6 +16240,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 16.2255,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun Raja Yogidas",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "47723536+Shubhranshu153@users.noreply.github.com",
+            "name": "Subhransu",
+            "username": "Shubhranshu153"
+          },
+          "distinct": true,
+          "id": "d9d125ca3a423b53405b4aeeb362d6ec1f133ba9",
+          "message": "chore: test multiple go versions in CI\n\nSigned-off-by: Arjun Raja Yogidas <arjunry@amazon.com>",
+          "timestamp": "2026-02-11T15:26:06-08:00",
+          "tree_id": "b212a72afeedf54340afaebcc1c7640b4558ba59",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d9d125ca3a423b53405b4aeeb362d6ec1f133ba9"
+        },
+        "date": 1770852956385,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 0.971,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0245,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 0.9490000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
