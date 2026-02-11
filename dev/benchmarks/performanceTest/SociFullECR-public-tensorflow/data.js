11a4832b3a9981dae720e91971e543daf88c8acf
github-action-benchmark <github@users.noreply.github.com>
2026-02-11 23:35:57 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d9d125ca3a423b53405b4aeeb362d6ec1f133ba9

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770843606997,
+  "lastUpdate": 1770852957786,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16282,6 +16282,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 2.045,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun Raja Yogidas",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "47723536+Shubhranshu153@users.noreply.github.com",
+            "name": "Subhransu",
+            "username": "Shubhranshu153"
+          },
+          "distinct": true,
+          "id": "d9d125ca3a423b53405b4aeeb362d6ec1f133ba9",
+          "message": "chore: test multiple go versions in CI\n\nSigned-off-by: Arjun Raja Yogidas <arjunry@amazon.com>",
+          "timestamp": "2026-02-11T15:26:06-08:00",
+          "tree_id": "b212a72afeedf54340afaebcc1c7640b4558ba59",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d9d125ca3a423b53405b4aeeb362d6ec1f133ba9"
+        },
+        "date": 1770852952782,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 21.264,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.701,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 4.498,
             "unit": "Seconds",
             "extra": "P90"
           }
