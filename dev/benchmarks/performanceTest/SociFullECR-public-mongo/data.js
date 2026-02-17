533c057d209948737fe9cb3f4a0f104be6054d11
github-action-benchmark <github@users.noreply.github.com>
2026-02-17 19:20:06 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 53346dda740518d886735e17a0c17e4c2ba7fd61

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771011726933,
+  "lastUpdate": 1771356006408,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16284,6 +16284,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.966,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hudson@cyzhu.com",
+            "name": "ChengyuZhu6",
+            "username": "ChengyuZhu6"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "53346dda740518d886735e17a0c17e4c2ba7fd61",
+          "message": "Add PrefetchConfig section\n\nAdd PrefetchConfig with enable flag and max_concurrency.\n\nFixes: #1832\n\nSigned-off-by: ChengyuZhu6 <hudson@cyzhu.com>",
+          "timestamp": "2026-02-17T11:10:45-08:00",
+          "tree_id": "cf0b190cb70c08058cec516c6e2e9eb9031b67dd",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/53346dda740518d886735e17a0c17e4c2ba7fd61"
+        },
+        "date": 1771355997487,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 2.4665,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3115,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.483,
             "unit": "Seconds",
             "extra": "P90"
           }
