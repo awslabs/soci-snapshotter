009ffd3613d14d1d2e2e46f5f9c4e5932ddeb77c
github-action-benchmark <github@users.noreply.github.com>
2026-02-20 18:50:15 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for b4baddbf466ba5b8a520ee7539b0d9fd051563d5

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771461394021,
+  "lastUpdate": 1771613415864,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16408,6 +16408,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 1.584,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "47723536+Shubhranshu153@users.noreply.github.com",
+            "name": "Subhransu",
+            "username": "Shubhranshu153"
+          },
+          "distinct": true,
+          "id": "b4baddbf466ba5b8a520ee7539b0d9fd051563d5",
+          "message": "Update push integration tests and Stabilize flakey tests\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-02-20T10:39:29-08:00",
+          "tree_id": "41227dde095422cb2311c6f2d7c229fa984d0940",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/b4baddbf466ba5b8a520ee7539b0d9fd051563d5"
+        },
+        "date": 1771613412687,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 0.7589999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.023,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.2445,
             "unit": "Seconds",
             "extra": "P90"
           }
