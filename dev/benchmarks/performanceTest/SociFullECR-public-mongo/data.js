7e1620afca27b8802a388e2654b1f11db6fca60e
github-action-benchmark <github@users.noreply.github.com>
2026-02-25 22:53:43 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 1619a75b168405b7239bd269a41c91130bebbc13

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771960626491,
+  "lastUpdate": 1772060023890,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16494,6 +16494,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.6795,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kazuyoshi.kato@baseten.co",
+            "name": "Kazuyoshi Kato",
+            "username": "kzys"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "1619a75b168405b7239bd269a41c91130bebbc13",
+          "message": "Fix TestSociCreateWithPrefetchArtifacts\n\nLogMonitor was updated in #1778, but this test was not updated to\nfollow the signatue change.\n\nSigned-off-by: Kazuyoshi Kato <kazuyoshi.kato@baseten.co>",
+          "timestamp": "2026-02-25T14:44:54-08:00",
+          "tree_id": "6e58af053021c3ea60a335b072c668e17a1b3e52",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/1619a75b168405b7239bd269a41c91130bebbc13"
+        },
+        "date": 1772060020323,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.874,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.306,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.3815,
             "unit": "Seconds",
             "extra": "P90"
           }
