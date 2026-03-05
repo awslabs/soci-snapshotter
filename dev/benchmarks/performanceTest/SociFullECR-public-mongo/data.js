26f611085db420cd0a847e6f46d8c7b16ebf7e70
github-action-benchmark <github@users.noreply.github.com>
2026-03-05 22:30:40 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for da566694278d89788e87209fe14770b49630c781

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772510533950,
+  "lastUpdate": 1772749840816,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16578,6 +16578,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.345,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "da566694278d89788e87209fe14770b49630c781",
+          "message": "Implement Standalone SOCI Image Convert\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-03-05T14:21:18-08:00",
+          "tree_id": "539151e87eaeb46311179e27fcb13dce3446f33d",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/da566694278d89788e87209fe14770b49630c781"
+        },
+        "date": 1772749838372,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 9.065000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.325,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.2035,
             "unit": "Seconds",
             "extra": "P90"
           }
