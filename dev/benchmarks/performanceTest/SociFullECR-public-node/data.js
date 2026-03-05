a2b3204f1811c65f81833b3500935dfceac73ecf
github-action-benchmark <github@users.noreply.github.com>
2026-03-05 22:30:48 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for da566694278d89788e87209fe14770b49630c781

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772510540251,
+  "lastUpdate": 1772749848316,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16454,6 +16454,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.9420000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "da566694278d89788e87209fe14770b49630c781",
+          "message": "Implement Standalone SOCI Image Convert\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-03-05T14:21:18-08:00",
+          "tree_id": "539151e87eaeb46311179e27fcb13dce3446f33d",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/da566694278d89788e87209fe14770b49630c781"
+        },
+        "date": 1772749841567,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 4.1185,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4785,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.872,
             "unit": "Seconds",
             "extra": "P90"
           }
