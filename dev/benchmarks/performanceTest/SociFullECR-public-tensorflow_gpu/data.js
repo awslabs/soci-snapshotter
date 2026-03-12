4091a4c188a2be7c18182fcf71de0c58b2c97301
github-action-benchmark <github@users.noreply.github.com>
2026-03-12 18:58:28 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d8e1a06e92793bf483a2eac576502137bc533525

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773245095365,
+  "lastUpdate": 1773341908596,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16664,6 +16664,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 1.974,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "d8e1a06e92793bf483a2eac576502137bc533525",
+          "message": "Remove containerd 1.6 from testing suite\n\nPer https://containerd.io/releases, 1.6 was EOL in August 2025 thus we\nhave no reason to keep testing against it.\n\n2.0 is also EOL but as it's a little newer it's probably fine to keep\ntesting against it anyway.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-03-12T11:43:31-07:00",
+          "tree_id": "a12c9793ad097c5c1f49d462bfcbf37adfd5adb3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d8e1a06e92793bf483a2eac576502137bc533525"
+        },
+        "date": 1773341895884,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 11.1975,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.8785,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 17.159,
             "unit": "Seconds",
             "extra": "P90"
           }
