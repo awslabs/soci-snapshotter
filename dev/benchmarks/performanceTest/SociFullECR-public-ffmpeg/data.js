2966953a0ab79decff854c7697b146f200dc4ee6
github-action-benchmark <github@users.noreply.github.com>
2026-03-19 22:11:38 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for dc2ed587e8142b1726e0578dfe21ec4c636e7bc5

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773341894926,
+  "lastUpdate": 1773958298025,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16829,6 +16829,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.7855,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "dc2ed587e8142b1726e0578dfe21ec4c636e7bc5",
+          "message": "Revert \"Skip grpc in bump-deps.sh\"\n\nThis reverts commit fe6994c8f582f2983b11898a41c98e3328f4bbb8.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-03-19T15:03:14-07:00",
+          "tree_id": "ae518531f74831ff0f5f7b40577205505c6c65db",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/dc2ed587e8142b1726e0578dfe21ec4c636e7bc5"
+        },
+        "date": 1773958295472,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.3155,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.008,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.8265,
             "unit": "Seconds",
             "extra": "P90"
           }
