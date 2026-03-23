c425fd829f6f2c83309b6805b3eb7dedc45e91b5
github-action-benchmark <github@users.noreply.github.com>
2026-03-23 20:07:38 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7b5763f1f76198b45f748e731a27e68f9933042a

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773958300978,
+  "lastUpdate": 1774296458928,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16912,6 +16912,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.9285,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "7b5763f1f76198b45f748e731a27e68f9933042a",
+          "message": "Upgrade gRPC to v1.79.3\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-03-23T12:54:14-07:00",
+          "tree_id": "24b38e5743fbbc867d12e23d01933a232681b254",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7b5763f1f76198b45f748e731a27e68f9933042a"
+        },
+        "date": 1774296451783,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 36.154,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.979,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 3.298,
             "unit": "Seconds",
             "extra": "P90"
           }
