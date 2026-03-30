fa95af7acb87bcc4d98e36d4f43da82005b5ac4e
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 18:11:01 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for bffea92ea85b48411c3a9216793e84c3058049a4

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774393784836,
+  "lastUpdate": 1774894261633,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16996,6 +16996,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 2.1352,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": true,
+          "id": "bffea92ea85b48411c3a9216793e84c3058049a4",
+          "message": "Skip snapshotter root path creation for standalone convert mode\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-03-30T11:00:16-07:00",
+          "tree_id": "4e645547f31b011fc5b9b9577758c5dd8fdf24cb",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/bffea92ea85b48411c3a9216793e84c3058049a4"
+        },
+        "date": 1774894259080,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 30.038,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.9898,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.5752,
             "unit": "Seconds",
             "extra": "P90"
           }
