fbc05d760216db4c9e06060abbc838f8b8c12df7
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 18:47:34 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d6eb44b6f0292b3a5d2a8217dd3675d2e810d019

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774894286309,
+  "lastUpdate": 1774896454209,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16832,6 +16832,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.3362,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "d6eb44b6f0292b3a5d2a8217dd3675d2e810d019",
+          "message": "Skip root path creation for standalone convert and use output dir for temp files\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-03-30T11:36:53-07:00",
+          "tree_id": "d935b570555527f866d1b87ae9ca1d23ac7de48e",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d6eb44b6f0292b3a5d2a8217dd3675d2e810d019"
+        },
+        "date": 1774896398495,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 1.504,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.47019999999999995,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.5936,
             "unit": "Seconds",
             "extra": "P90"
           }
