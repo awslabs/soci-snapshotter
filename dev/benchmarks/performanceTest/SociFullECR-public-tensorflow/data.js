c2653f3c365103163c7027862b1be40875c8f219
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 21:19:15 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 8f363ceccda9f00442061e1f307509c3b63a6ee7

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774896426634,
+  "lastUpdate": 1774905555642,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17080,6 +17080,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 2.2564,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "8f363ceccda9f00442061e1f307509c3b63a6ee7",
+          "message": "Add root testing to unit test workflows\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-03-30T14:10:24-07:00",
+          "tree_id": "790601f3586a3bb1de5b770d7c986f5192782b29",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/8f363ceccda9f00442061e1f307509c3b63a6ee7"
+        },
+        "date": 1774905540811,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 9.2792,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.749,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 1.9853999999999998,
             "unit": "Seconds",
             "extra": "P90"
           }
