373ab92b2b1e78b8df2809abf1345679df1bb033
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 18:47:31 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d6eb44b6f0292b3a5d2a8217dd3675d2e810d019

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774894255065,
+  "lastUpdate": 1774896451089,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -16874,6 +16874,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 3.2868000000000004,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "d6eb44b6f0292b3a5d2a8217dd3675d2e810d019",
+          "message": "Skip root path creation for standalone convert and use output dir for temp files\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-03-30T11:36:53-07:00",
+          "tree_id": "d935b570555527f866d1b87ae9ca1d23ac7de48e",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d6eb44b6f0292b3a5d2a8217dd3675d2e810d019"
+        },
+        "date": 1774896397554,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 24.3062,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.6879999999999997,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 4.5834,
             "unit": "Seconds",
             "extra": "P90"
           }
