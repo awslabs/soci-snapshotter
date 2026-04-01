165b657cb032a38149ba90438f919441942a1cc0
github-action-benchmark <github@users.noreply.github.com>
2026-04-01 20:24:18 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 0a1984bb2ffb169a0d11d543a6dde85dfd6ba506

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774996472440,
+  "lastUpdate": 1775075058013,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17248,6 +17248,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 2.0632,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "0a1984bb2ffb169a0d11d543a6dde85dfd6ba506",
+          "message": "Fast-fail on incorrect MaxSpanID\n\nAn incorrect MaxSpanID should fail much faster since we already have\nthe info we need to make this determination when creating a new\nSpanManager.\n\nThis should cover any failures in accessing the data in\nverifySpanContents, but figured it can't hurt to add an extra sanity\ncheck in the func to be extra sure.\n\nThis change required reworking the SpanManager's New function to handle\nfast fails, so this commit also covers all the changes needed for that.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-04-01T13:14:32-07:00",
+          "tree_id": "02c756b5776f6b6b75821798fdddd7c515f8a6c1",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/0a1984bb2ffb169a0d11d543a6dde85dfd6ba506"
+        },
+        "date": 1775075051818,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 11.7972,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.6637999999999997,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.3102,
             "unit": "Seconds",
             "extra": "P90"
           }
