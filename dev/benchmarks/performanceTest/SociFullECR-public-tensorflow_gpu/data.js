1dac1e598962ad377d82f79ee89cad890c6c7a4f
github-action-benchmark <github@users.noreply.github.com>
2026-04-11 01:22:57 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for ad6c319baba9bcd99f7d12517aef095e9b6d7696

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775075287752,
+  "lastUpdate": 1775870577018,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17168,6 +17168,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 3.2352,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": true,
+          "id": "ad6c319baba9bcd99f7d12517aef095e9b6d7696",
+          "message": "Bump docker/library/registry from 3.0.0 to 3.1.0\n\nBumps docker/library/registry from 3.0.0 to 3.1.0.\n\n---\nupdated-dependencies:\n- dependency-name: docker/library/registry\n  dependency-version: 3.1.0\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-04-10T18:14:03-07:00",
+          "tree_id": "a5f792346ccbe957176fbdb8817925f6d85fd1c3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/ad6c319baba9bcd99f7d12517aef095e9b6d7696"
+        },
+        "date": 1775870573441,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 23.859400000000004,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.7178,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.4006,
             "unit": "Seconds",
             "extra": "P90"
           }
