c573af3bea361dfb5e33c6e15f215cb0c8cc06c9
github-action-benchmark <github@users.noreply.github.com>
2026-04-11 01:22:55 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for ad6c319baba9bcd99f7d12517aef095e9b6d7696

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775075286237,
+  "lastUpdate": 1775870575168,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17126,6 +17126,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 4.6494,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": true,
+          "id": "ad6c319baba9bcd99f7d12517aef095e9b6d7696",
+          "message": "Bump docker/library/registry from 3.0.0 to 3.1.0\n\nBumps docker/library/registry from 3.0.0 to 3.1.0.\n\n---\nupdated-dependencies:\n- dependency-name: docker/library/registry\n  dependency-version: 3.1.0\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-04-10T18:14:03-07:00",
+          "tree_id": "a5f792346ccbe957176fbdb8817925f6d85fd1c3",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/ad6c319baba9bcd99f7d12517aef095e9b6d7696"
+        },
+        "date": 1775870572846,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 1.542,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.4848,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.1916,
             "unit": "Seconds",
             "extra": "P90"
           }
