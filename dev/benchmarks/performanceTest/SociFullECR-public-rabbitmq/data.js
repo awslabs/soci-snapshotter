05bf955109385d1c8bd10914bb5ada60dce18c3a
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 23:46:54 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for ccf4c148ae4b46a387ba8093e019102b12a62bf9

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776200631597,
+  "lastUpdate": 1777679214778,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17253,6 +17253,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.2622,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "ccf4c148ae4b46a387ba8093e019102b12a62bf9",
+          "message": "Upgrade to go 1.25.9\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-05-01T16:34:29-07:00",
+          "tree_id": "d857d78dcdc3f94ee4cedec66b84173e514afb90",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/ccf4c148ae4b46a387ba8093e019102b12a62bf9"
+        },
+        "date": 1777679212085,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 19.0778,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 9.6308,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 2.5972,
             "unit": "Seconds",
             "extra": "P90"
           }
