e09a3195805849fbe886953d3c68ddef9a3eeec3
github-action-benchmark <github@users.noreply.github.com>
2026-05-07 16:29:05 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 8b759752a5335f27a998bb09586346f9f6a8580e

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778129913084,
+  "lastUpdate": 1778171345712,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17459,6 +17459,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.1758,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "distinct": true,
+          "id": "8b759752a5335f27a998bb09586346f9f6a8580e",
+          "message": "Fix multi-platform soci standalone convert\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-05-07T09:20:25-07:00",
+          "tree_id": "7a53002445691f31f1b33db282285a5d8f518ece",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/8b759752a5335f27a998bb09586346f9f6a8580e"
+        },
+        "date": 1778171342533,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0126,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.1932,
             "unit": "Seconds",
             "extra": "P90"
           }
