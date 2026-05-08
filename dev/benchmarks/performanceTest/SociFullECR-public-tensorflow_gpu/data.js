5bfcaae00e657dd1c4106c2fc7a20223c5a1c0a9
github-action-benchmark <github@users.noreply.github.com>
2026-05-08 22:36:21 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 77aef7c2e17f7146f4eab4e58d5935a8b264a2ef

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778279135297,
+  "lastUpdate": 1778279781119,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17546,6 +17546,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 3.3486000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "77aef7c2e17f7146f4eab4e58d5935a8b264a2ef",
+          "message": "Bump softprops/action-gh-release from 2 to 3\n\nBumps [softprops/action-gh-release](https://github.com/softprops/action-gh-release) from 2 to 3.\n- [Release notes](https://github.com/softprops/action-gh-release/releases)\n- [Changelog](https://github.com/softprops/action-gh-release/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/softprops/action-gh-release/compare/v2...v3)\n\n---\nupdated-dependencies:\n- dependency-name: softprops/action-gh-release\n  dependency-version: '3'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-05-08T15:07:11-07:00",
+          "tree_id": "70d1c0e5ba34ebf3bc9d582467324818e9f9bc36",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/77aef7c2e17f7146f4eab4e58d5935a8b264a2ef"
+        },
+        "date": 1778279777792,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 53.0218,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.3718,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 5.1966,
             "unit": "Seconds",
             "extra": "P90"
           }
