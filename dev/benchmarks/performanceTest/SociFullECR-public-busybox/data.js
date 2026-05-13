f971fea2b468e3131a2340a5aa3d5554d9d181c3
github-action-benchmark <github@users.noreply.github.com>
2026-05-13 19:45:08 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 28b990bb3f58a36c66e5f9bc5e7ea342a065e6f0

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778279775526,
+  "lastUpdate": 1778701508738,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17669,6 +17669,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 3.2476,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "28b990bb3f58a36c66e5f9bc5e7ea342a065e6f0",
+          "message": "Add minimal crictl integration test + setup\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-05-13T12:26:36-07:00",
+          "tree_id": "7b4b9046f1c9aba768293ec99b610a2a9ab30dc7",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/28b990bb3f58a36c66e5f9bc5e7ea342a065e6f0"
+        },
+        "date": 1778701501330,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.035,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.1324,
             "unit": "Seconds",
             "extra": "P90"
           }
