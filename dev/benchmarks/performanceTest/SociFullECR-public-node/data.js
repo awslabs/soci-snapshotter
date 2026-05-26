e16774a5f01936a4b73d831274ebc26016c27957
github-action-benchmark <github@users.noreply.github.com>
2026-05-26 15:20:42 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 6d0eff350e1e166cdfdb6a3407e158e4393aa9e0

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778701924281,
+  "lastUpdate": 1779808842455,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17672,6 +17672,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.6508,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "6d0eff350e1e166cdfdb6a3407e158e4393aa9e0",
+          "message": "Bump dependencies using scripts/bump-deps.sh\n\nSigned-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2026-05-26T11:08:33-04:00",
+          "tree_id": "659da1cc3a3097a3e4e7d661a10c8e9fbd120d37",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/6d0eff350e1e166cdfdb6a3407e158e4393aa9e0"
+        },
+        "date": 1779808837685,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 5.7456,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.7142,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 4.510400000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
