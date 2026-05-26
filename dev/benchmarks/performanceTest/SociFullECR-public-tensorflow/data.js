04f086b690a3726ce64deebb1246263afb182c00
github-action-benchmark <github@users.noreply.github.com>
2026-05-26 19:42:39 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 560e2ab6dbfa00b938767ed024de6f06b727f545

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779808844375,
+  "lastUpdate": 1779824559113,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17920,6 +17920,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 3.9272,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "560e2ab6dbfa00b938767ed024de6f06b727f545",
+          "message": "Support concatenated gzip (mgzip/pigz) in ztoc generation and extraction\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-05-26T15:31:42-04:00",
+          "tree_id": "65e0b074a1db2963ff94f69d2f08a44f99941925",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/560e2ab6dbfa00b938767ed024de6f06b727f545"
+        },
+        "date": 1779824556545,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 38.045,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.8806,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.4859999999999998,
             "unit": "Seconds",
             "extra": "P90"
           }
