ee84ae084f08ef5e1bbd9bb945493f2216d6cda6
github-action-benchmark <github@users.noreply.github.com>
2026-05-26 19:42:44 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 560e2ab6dbfa00b938767ed024de6f06b727f545

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779808851166,
+  "lastUpdate": 1779824564043,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17879,6 +17879,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 3.6082,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "560e2ab6dbfa00b938767ed024de6f06b727f545",
+          "message": "Support concatenated gzip (mgzip/pigz) in ztoc generation and extraction\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-05-26T15:31:42-04:00",
+          "tree_id": "65e0b074a1db2963ff94f69d2f08a44f99941925",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/560e2ab6dbfa00b938767ed024de6f06b727f545"
+        },
+        "date": 1779824557546,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1408,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.0096,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.9276,
             "unit": "Seconds",
             "extra": "P90"
           }
