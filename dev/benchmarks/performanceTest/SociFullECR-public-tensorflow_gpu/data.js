dff56e145a3814fc032dac0750725e81a9b259cd
github-action-benchmark <github@users.noreply.github.com>
2026-05-26 23:05:45 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for e0533d1d57c652b6c69bf37a3d623c30e29f59c8

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779836685093,
+  "lastUpdate": 1779836745047,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -17840,6 +17840,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.1786,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "e0533d1d57c652b6c69bf37a3d623c30e29f59c8",
+          "message": "Upgrade containerd versions in CI\n\nMore than a version bump so worth adding details.\n\nPer https://containerd.io/releases/, 2.0 is now LTS and 2.1 is EOL. So\nwe should start to test on 2.0 instead of 2.1. Additionally, 2.3 has\ncome out recently so we should start testing against it.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-05-26T18:56:02-04:00",
+          "tree_id": "9fd89bf78ef3e39b7c3e0fdcd6d93b2be85cdfb8",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/e0533d1d57c652b6c69bf37a3d623c30e29f59c8"
+        },
+        "date": 1779836742482,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 34.7226,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.7882000000000002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.6846,
             "unit": "Seconds",
             "extra": "P90"
           }
