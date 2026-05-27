d6ff9aa282d1eea309d85d4d660af9835dc38acc
github-action-benchmark <github@users.noreply.github.com>
2026-05-27 18:30:26 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for d547bc687817fb134fbeee5780f9b1b9c8db0ee3

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779906528627,
+  "lastUpdate": 1779906626688,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -18047,6 +18047,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.4334,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "d547bc687817fb134fbeee5780f9b1b9c8db0ee3",
+          "message": "Enforce monotonic checkpoint offsets\n\nOur zTOC logic assumes monotonic checkpoint offsets for its logic to\nchange but never enforced it. This change enforces this logic by forcing\nstrictly increasing start and end of checkpoints.\n\nThere's technically no reason a checkpoint can't be zero length so we\ncan maybe make the change to allow that but it's probably more sensical\nto not allow it to begin with.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-05-27T14:20:59-04:00",
+          "tree_id": "fbe3dec3790c0d0bb4a90d5e56d293c56ad1e45c",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/d547bc687817fb134fbeee5780f9b1b9c8db0ee3"
+        },
+        "date": 1779906619725,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.149,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.009,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 2.0696,
             "unit": "Seconds",
             "extra": "P90"
           }
