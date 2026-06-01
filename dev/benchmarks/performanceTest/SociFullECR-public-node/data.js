d7cd134eb950c70bdfa0c69c4704a0bfa6046f4d
github-action-benchmark <github@users.noreply.github.com>
2026-06-01 23:10:29 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 2116e1d1d295556e523306037db16dd78b1f187c

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779920871507,
+  "lastUpdate": 1780355429382,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -18008,6 +18008,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.028,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "2116e1d1d295556e523306037db16dd78b1f187c",
+          "message": "Upgrade to containerd v2.2.4\n\nv2.3.1 (latest as of this commit) requires Go 1.26 which is why we chose\nto use v2.2.4 instead. This should be an easy upgrade to 2.3 anyway\nshould we choose to upgrade it down the line.\n\nThis naturally means we are also upgrading a ton of transitive\ndependencies (as this is an update we have been putting off for a while)\nso hoping this won't break anything too major...\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-06-01T16:00:43-07:00",
+          "tree_id": "ba605e9c314d0ee3cbb57428cf5c223ee3e4c2c6",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/2116e1d1d295556e523306037db16dd78b1f187c"
+        },
+        "date": 1780355426602,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 2.1236,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.525,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.0558,
             "unit": "Seconds",
             "extra": "P90"
           }
