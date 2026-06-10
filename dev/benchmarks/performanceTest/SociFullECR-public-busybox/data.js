74ec4a81765b7ba841c0f36b173dac385fb5cbfc
github-action-benchmark <github@users.noreply.github.com>
2026-06-10 22:27:00 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for b1f378df10f4cd9372ff8409c96248e4e5bcf9b5

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781130145239,
+  "lastUpdate": 1781130420688,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -18383,6 +18383,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.4438,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun Raja Yogidas",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": true,
+          "id": "b1f378df10f4cd9372ff8409c96248e4e5bcf9b5",
+          "message": "run go mod tidy\n\nSigned-off-by: Arjun Raja Yogidas <arjunry@amazon.com>",
+          "timestamp": "2026-06-10T15:12:25-07:00",
+          "tree_id": "4d25eea2a27387fe805a95ec876d03aa71ad139a",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/b1f378df10f4cd9372ff8409c96248e4e5bcf9b5"
+        },
+        "date": 1781130417797,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.015,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.007,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.8117999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
