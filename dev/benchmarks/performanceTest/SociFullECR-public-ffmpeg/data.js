e014db54b9ca6aeac44e1801943d8a638c577776
github-action-benchmark <github@users.noreply.github.com>
2026-06-22 17:39:57 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for bea655f35ac1755bfdd96ffd1fe6ab93e2560af5

--- dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-ffmpeg/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781912435636,
+  "lastUpdate": 1782149997024,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -18677,6 +18677,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
             "value": 1.8075999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "bea655f35ac1755bfdd96ffd1fe6ab93e2560af5",
+          "message": "Add checks before generating a release\n\nIdeally we would want to run checks before pushing a tag, however the\ninfra required for the GPG key does not currently exist and would\nrequire a revamp of our existing architecture. So this is the stop-gap\nsolution I've arrived at, which will let maintainers push a tag but not\nallow the release to be created if the requisite checks don't pass.\n\nThis isn't an amazing solution but having a dead tag floating around for\na couple of minutes is definitely better than missing release artifacts.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-06-22T13:29:49-04:00",
+          "tree_id": "a4b1672c32612e509c167e83b1ad9863d772a9f9",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/bea655f35ac1755bfdd96ffd1fe6ab93e2560af5"
+        },
+        "date": 1782149986225,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.1392,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskDuration",
+            "value": 0.0096,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullTaskDuration",
+            "value": 1.9274,
             "unit": "Seconds",
             "extra": "P90"
           }
