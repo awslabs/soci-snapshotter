562a68fa771b7c4397e45a2e9c885cef1674b52b
github-action-benchmark <github@users.noreply.github.com>
2026-06-23 21:22:52 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 65c76c8ccac3e4f308e811ba6a76a93dc5f3048c

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782153462639,
+  "lastUpdate": 1782249772284,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -18844,6 +18844,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 2.3092,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "65c76c8ccac3e4f308e811ba6a76a93dc5f3048c",
+          "message": "Bump dependencies using scripts/bump-deps.sh\n\nThis includes a manual change to the config, as go-toml had an update to\nthe TOML output. Very small change but needed for checks to pass.\n\nSigned-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2026-06-23T14:12:07-07:00",
+          "tree_id": "01110549a61d86e97e779274837a9fe64140b7a4",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/65c76c8ccac3e4f308e811ba6a76a93dc5f3048c"
+        },
+        "date": 1782249762629,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 25.973800000000004,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.9139999999999997,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.6318,
             "unit": "Seconds",
             "extra": "P90"
           }
