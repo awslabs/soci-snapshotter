bddf84edd6a79db411d959d6aecf59703d9686da
github-action-benchmark <github@users.noreply.github.com>
2026-07-15 21:44:04 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7e80b16d224dbe4b1025c8d83b13d8948703dee9

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782249776335,
+  "lastUpdate": 1784151844357,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -18804,6 +18804,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.5386,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "7e80b16d224dbe4b1025c8d83b13d8948703dee9",
+          "message": "Don't preserve tar ownership in standalone extraction\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-07-15T14:35:46-07:00",
+          "tree_id": "fbdf15cf309ce3d4f5abd65c3d14c6a2a198690d",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7e80b16d224dbe4b1025c8d83b13d8948703dee9"
+        },
+        "date": 1784151839964,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 1.6119999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.6386000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 1.6927999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
