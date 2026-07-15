613abb88ba5a237f65580daf9f36f3d465a3266c
github-action-benchmark <github@users.noreply.github.com>
2026-07-15 21:44:14 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7e80b16d224dbe4b1025c8d83b13d8948703dee9

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782249768254,
+  "lastUpdate": 1784151854562,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -18680,6 +18680,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.2652,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "7e80b16d224dbe4b1025c8d83b13d8948703dee9",
+          "message": "Don't preserve tar ownership in standalone extraction\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-07-15T14:35:46-07:00",
+          "tree_id": "fbdf15cf309ce3d4f5abd65c3d14c6a2a198690d",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7e80b16d224dbe4b1025c8d83b13d8948703dee9"
+        },
+        "date": 1784151843537,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 1.2762,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.42219999999999996,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.1048,
             "unit": "Seconds",
             "extra": "P90"
           }
