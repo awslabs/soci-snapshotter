8376eb19f684b5c53cc2294363fd917427f6f3c1
github-action-benchmark <github@users.noreply.github.com>
2026-07-24 17:00:42 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for adf5ba912dc66b35dba71acb40facb72576040c9

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784581645778,
+  "lastUpdate": 1784912442312,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19097,6 +19097,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.7328000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "ayushkp@amazon.com",
+            "name": "ayush-panta",
+            "username": "ayush-panta"
+          },
+          "distinct": true,
+          "id": "adf5ba912dc66b35dba71acb40facb72576040c9",
+          "message": "Fix up workflow scripts\n\nAlso fixes an incorrect regex for create-release-branch.yml\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-07-24T09:52:13-07:00",
+          "tree_id": "d259d79ab55fa4085f87694ae8c48f0cf7daa93d",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/adf5ba912dc66b35dba71acb40facb72576040c9"
+        },
+        "date": 1784912435445,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.0136,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.0066,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 0.6779999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
