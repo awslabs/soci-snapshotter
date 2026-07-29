6f0ed8ff7d8db5271d4ba55e878c443ee1b0b7c2
github-action-benchmark <github@users.noreply.github.com>
2026-07-29 21:58:59 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for cdee6469bb7fcb310124084cfff9d1e157a92890

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784930897790,
+  "lastUpdate": 1785362339308,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19226,6 +19226,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 1.8452,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "ayushkp@amazon.com",
+            "name": "ayush-panta",
+            "username": "ayush-panta"
+          },
+          "distinct": true,
+          "id": "cdee6469bb7fcb310124084cfff9d1e157a92890",
+          "message": "Dockerfile: skip unnecessary builds\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-07-29T14:47:44-07:00",
+          "tree_id": "6fe5e4804a6eb4077f46a96b0d29e698b186109e",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/cdee6469bb7fcb310124084cfff9d1e157a92890"
+        },
+        "date": 1785362332549,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 38.293,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.9792,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 2.746,
             "unit": "Seconds",
             "extra": "P90"
           }
