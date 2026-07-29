711a7c25a372186e2deb5778341dc44523d11305
github-action-benchmark <github@users.noreply.github.com>
2026-07-29 23:07:49 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 6049a428a59a14c74aa6ed0fcf2ba8525eaa1d42

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785366214074,
+  "lastUpdate": 1785366469627,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19394,6 +19394,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.352,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "ayushkp@amazon.com",
+            "name": "ayush-panta",
+            "username": "ayush-panta"
+          },
+          "distinct": true,
+          "id": "6049a428a59a14c74aa6ed0fcf2ba8525eaa1d42",
+          "message": "Propagate Docker Compose errors\n\nThe existing workflow already only outputs errors if we get any, so it's\nfine to just use progress=plain instead of -q as our happy path workflow\nremains unchanged. However this will now make it much easier to debug\nany errors when building the Dockerfile.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-07-29T15:54:20-07:00",
+          "tree_id": "652eb8cd5cefcd60e8fd0760b435068d418627fc",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/6049a428a59a14c74aa6ed0fcf2ba8525eaa1d42"
+        },
+        "date": 1785366463969,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 75.6122,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.7046,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 3.298,
             "unit": "Seconds",
             "extra": "P90"
           }
