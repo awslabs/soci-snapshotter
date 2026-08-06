62e35feab43879867f8a241824f4c556ee09e4b7
github-action-benchmark <github@users.noreply.github.com>
2026-08-06 22:32:27 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for ce493448a37085aca00ec114d4d9e1e1341bb402

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785435333049,
+  "lastUpdate": 1786055547094,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19520,6 +19520,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.1414,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "ce493448a37085aca00ec114d4d9e1e1341bb402",
+          "message": "Reduce redundant sizes in unit tests to make it faster\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-08-06T14:40:34-07:00",
+          "tree_id": "6ccb7498d72bda7196d19ffb7fc3a110514db803",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/ce493448a37085aca00ec114d4d9e1e1341bb402"
+        },
+        "date": 1786055542460,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 15.622400000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 8.643600000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 2.0282,
             "unit": "Seconds",
             "extra": "P90"
           }
