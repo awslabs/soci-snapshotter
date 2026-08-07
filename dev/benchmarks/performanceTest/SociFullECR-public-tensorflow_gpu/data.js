3a4a5ebdb3cc5014527edd6674ca4051befec349
github-action-benchmark <github@users.noreply.github.com>
2026-08-07 21:13:59 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 9c4e0f4bf5bade3d44974fb1273f18355649ffc0

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786137228842,
+  "lastUpdate": 1786137239090,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19645,6 +19645,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 4.994,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "9c4e0f4bf5bade3d44974fb1273f18355649ffc0",
+          "message": "Remove timeout and reduce redundant sizes in unit tests to make it faster\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-08-07T14:04:44-07:00",
+          "tree_id": "d76db60c91d67b56e2155d32319b6cec7a5b403e",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/9c4e0f4bf5bade3d44974fb1273f18355649ffc0"
+        },
+        "date": 1786137235972,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 19.6748,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.3338,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 3.7582,
             "unit": "Seconds",
             "extra": "P90"
           }
