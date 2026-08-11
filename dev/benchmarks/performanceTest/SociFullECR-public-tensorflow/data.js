59c3847432ab4a838bf034fe516113b557ac1409
github-action-benchmark <github@users.noreply.github.com>
2026-08-11 18:35:15 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f38822e7f85f8a442bfc8db2ca6baeb4bbaa4d6e

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786137235685,
+  "lastUpdate": 1786473315240,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19851,6 +19851,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 2.3252,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "distinct": true,
+          "id": "f38822e7f85f8a442bfc8db2ca6baeb4bbaa4d6e",
+          "message": "Parallelize tests and reduce redundant sizes\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-08-11T11:08:48-07:00",
+          "tree_id": "81affe7c83dd20210bd246396eddc056c7e2a663",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f38822e7f85f8a442bfc8db2ca6baeb4bbaa4d6e"
+        },
+        "date": 1786473303921,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 34.2564,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.7844,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.0496,
             "unit": "Seconds",
             "extra": "P90"
           }
