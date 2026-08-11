7ff625d35e25da921435f6aab658d64dc1fd8f96
github-action-benchmark <github@users.noreply.github.com>
2026-08-11 18:35:07 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for f38822e7f85f8a442bfc8db2ca6baeb4bbaa4d6e

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786137248395,
+  "lastUpdate": 1786473307196,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19645,6 +19645,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.8982,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "swpnlg@amazon.com",
+            "name": "Swapnanil Gupta",
+            "username": "Swapnanil-Gupta"
+          },
+          "distinct": true,
+          "id": "f38822e7f85f8a442bfc8db2ca6baeb4bbaa4d6e",
+          "message": "Parallelize tests and reduce redundant sizes\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-08-11T11:08:48-07:00",
+          "tree_id": "81affe7c83dd20210bd246396eddc056c7e2a663",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/f38822e7f85f8a442bfc8db2ca6baeb4bbaa4d6e"
+        },
+        "date": 1786473304044,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 2.0174000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.5002,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.9686000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
