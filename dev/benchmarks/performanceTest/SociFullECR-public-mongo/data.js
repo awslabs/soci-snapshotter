ab86afaf11fde22668ee890417f3cc2e7973f8af
github-action-benchmark <github@users.noreply.github.com>
2026-09-02 22:00:04 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 2292ec7e133885b0a233846fc7b5bf7620c3fd0c

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788383366122,
+  "lastUpdate": 1788386404126,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19937,6 +19937,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 2.5574,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prafulgupta6@gmail.com",
+            "name": "Praful Gupta",
+            "username": "prafgup"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "2292ec7e133885b0a233846fc7b5bf7620c3fd0c",
+          "message": "Parallelize more unit tests and reduce redundant spans\n\nSigned-off-by: Praful Gupta <prafulgupta6@gmail.com>",
+          "timestamp": "2026-09-02T14:44:14-07:00",
+          "tree_id": "ebe24919e246f24c6224eed903220a409ed7c25a",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/2292ec7e133885b0a233846fc7b5bf7620c3fd0c"
+        },
+        "date": 1788386394532,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 20.949400000000004,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3304,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 2.2614,
             "unit": "Seconds",
             "extra": "P90"
           }
