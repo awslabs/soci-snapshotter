be419c0174231daf3cc366e669a438864204bcf9
github-action-benchmark <github@users.noreply.github.com>
2026-09-02 21:09:26 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for cfae695e36c19fbee8034f2ff008cad7f8d059f9

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786493540220,
+  "lastUpdate": 1788383366122,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19895,6 +19895,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 2.1274,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "cfae695e36c19fbee8034f2ff008cad7f8d059f9",
+          "message": "Ignore license check for go-metrics\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-09-02T13:59:07-07:00",
+          "tree_id": "58f3667ca98baaf9f88b082d1f382959f09ac89b",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/cfae695e36c19fbee8034f2ff008cad7f8d059f9"
+        },
+        "date": 1788383349199,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 7.392799999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.3206,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 2.5574,
             "unit": "Seconds",
             "extra": "P90"
           }
