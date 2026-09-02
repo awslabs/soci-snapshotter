e586475ff931b02f5b1715dc2ec19345303de421
github-action-benchmark <github@users.noreply.github.com>
2026-09-02 21:09:20 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for cfae695e36c19fbee8034f2ff008cad7f8d059f9

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786493542681,
+  "lastUpdate": 1788383360646,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19935,6 +19935,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 2.6514,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "cfae695e36c19fbee8034f2ff008cad7f8d059f9",
+          "message": "Ignore license check for go-metrics\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-09-02T13:59:07-07:00",
+          "tree_id": "58f3667ca98baaf9f88b082d1f382959f09ac89b",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/cfae695e36c19fbee8034f2ff008cad7f8d059f9"
+        },
+        "date": 1788383349565,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 12.1102,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.7066,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 3.0914,
             "unit": "Seconds",
             "extra": "P90"
           }
