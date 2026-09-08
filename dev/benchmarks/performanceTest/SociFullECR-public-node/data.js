bd0086dac11e0f68a080489306adbdd9cb9801eb
github-action-benchmark <github@users.noreply.github.com>
2026-09-08 20:33:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 31bfe86b42c96b6d829ad079567666e1a818cb1f

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788472945801,
+  "lastUpdate": 1788899602890,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19855,6 +19855,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.951,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "31bfe86b42c96b6d829ad079567666e1a818cb1f",
+          "message": "Upgrade golangci-lint to 2.12.2\n\nThe last version of golangci-lint didn't support Go 1.26, so upgrading\nit.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-09-08T13:22:43-07:00",
+          "tree_id": "632b63c9cc866db1e86d37929e105e9fdc78c1d7",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/31bfe86b42c96b6d829ad079567666e1a818cb1f"
+        },
+        "date": 1788899588353,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 4.5316,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.493,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.847,
             "unit": "Seconds",
             "extra": "P90"
           }
