613151b041e1eb68a2d78401630d6b476375647d
github-action-benchmark <github@users.noreply.github.com>
2026-09-08 23:35:04 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 27a4f87e50c3ee6040d798f86b5bce5924f58de4

--- dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-busybox/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788899595720,
+  "lastUpdate": 1788910504171,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -20020,6 +20020,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-busybox-pullTaskDuration",
             "value": 1.0293999999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "27a4f87e50c3ee6040d798f86b5bce5924f58de4",
+          "message": "Use WatchFuncWithContext\n\nThis technically uses a different context from before, but this is\nprobably more correct behavior.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-09-08T16:24:41-07:00",
+          "tree_id": "4e0ff5c31e950db6c50e725f06eb50cb3369d776",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/27a4f87e50c3ee6040d798f86b5bce5924f58de4"
+        },
+        "date": 1788910496573,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-busybox-lazyTaskDuration",
+            "value": 0.014,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-localTaskDuration",
+            "value": 0.0076,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-busybox-pullTaskDuration",
+            "value": 1.1888,
             "unit": "Seconds",
             "extra": "P90"
           }
