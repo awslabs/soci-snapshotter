a36147c6cc2dea106402463c70fbf33a989e5180
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 23:02:27 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7a806678f0b733660bbf0e07b61833b08ff9a26f

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789497417446,
+  "lastUpdate": 1789513347790,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -20187,6 +20187,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 0.7662,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rdu@amazon.com",
+            "name": "Radu Constantinescu"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "7a806678f0b733660bbf0e07b61833b08ff9a26f",
+          "message": "feat: Add custom snapshot request headers\n\nAllow snapshot labels to add validated HTTP headers to registry requests. Protect snapshotter-owned headers and propagate caller headers through fetcher creation and request execution.\n\nSigned-off-by: Radu Constantinescu <rdu@amazon.com>",
+          "timestamp": "2026-09-15T15:52:13-07:00",
+          "tree_id": "643955239e339910dde11b074cb64b49b298b421",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7a806678f0b733660bbf0e07b61833b08ff9a26f"
+        },
+        "date": 1789513343781,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 1.1228,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0282,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 1.2566,
             "unit": "Seconds",
             "extra": "P90"
           }
