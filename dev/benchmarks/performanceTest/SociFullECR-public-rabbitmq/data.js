5b9aa54880ae5fdc45e61d37920be919f5c4fce3
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 18:37:00 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 9569b301884a8057db71de75074096201304eae1

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788978432613,
+  "lastUpdate": 1789497420586,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -19982,6 +19982,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 2.1236,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "9569b301884a8057db71de75074096201304eae1",
+          "message": "Fix daemon crash in parallel pull\n\nThis is a slight redesign of the parallel pull workflow, where the crux\nof the issue lies in a nil dereference between when an image is getting\nready for a pull operation, and when it starts to premount. As there's\nno lock that stays in place between premounting and getting an image\nlayer job, there was a race that would cause it to panic.\n\nThis specific fix is solvable enough via better locking mechanisms,\nwhich this does fix, however there's a lot of messy contention here, in\nparticular with pulling the same image. We operate off the assumption\nthat the same diffID will never be in-flight in SOCI (CRI has an option\nfor this), but it doesn't stop separate requests from being\nde-duplicated, which is why we have a layerJob array instead of just a\nsingular one per image pull. Solving this means separating out contexts\nto each request instead of just the one per image job, which required a\nwhole restructuring. This should hopefully allow concurrent operations\nto both attempt an image pull, instead of one failure killing the whole\nimage operation.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2026-09-15T11:17:31-07:00",
+          "tree_id": "7402f9d01582947e5585243031170b79d7a07a4c",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/9569b301884a8057db71de75074096201304eae1"
+        },
+        "date": 1789497415316,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 15.3656,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 10.671,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 4.3024000000000004,
             "unit": "Seconds",
             "extra": "P90"
           }
