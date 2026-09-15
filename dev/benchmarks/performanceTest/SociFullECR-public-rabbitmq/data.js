5544a76873ab2771dca549e600fc78967fa09ccd
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 23:02:13 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7a806678f0b733660bbf0e07b61833b08ff9a26f

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789497420586,
+  "lastUpdate": 1789513333692,
   "repoUrl": "https://github.com/awslabs/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -20024,6 +20024,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 4.3024000000000004,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rdu@amazon.com",
+            "name": "Radu Constantinescu"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "7a806678f0b733660bbf0e07b61833b08ff9a26f",
+          "message": "feat: Add custom snapshot request headers\n\nAllow snapshot labels to add validated HTTP headers to registry requests. Protect snapshotter-owned headers and propagate caller headers through fetcher creation and request execution.\n\nSigned-off-by: Radu Constantinescu <rdu@amazon.com>",
+          "timestamp": "2026-09-15T15:52:13-07:00",
+          "tree_id": "643955239e339910dde11b074cb64b49b298b421",
+          "url": "https://github.com/awslabs/soci-snapshotter/commit/7a806678f0b733660bbf0e07b61833b08ff9a26f"
+        },
+        "date": 1789513330545,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 14.8254,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 10.3604,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 1.3317999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
